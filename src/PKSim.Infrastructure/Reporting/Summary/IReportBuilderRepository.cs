1d7ca11a376906a674d59182cd01c5c2e3e032ae
Michael Sevestre <michael@design2code.ca>
2017-06-01 14:35:51 -0400

    Fixes #207 tool top not visible in tree view (#208)

--- src/PKSim.Infrastructure/Reporting/Summary/IReportBuilderRepository.cs
+++ src/PKSim.Infrastructure/Reporting/Summary/IReportBuilderRepository.cs
@@ -3,11 +3,11 @@ using OSPSuite.Utility.Container;
 
 namespace PKSim.Infrastructure.Reporting.Summary
 {
-   public interface IReportBuilderRepository:IBuilderRepository<IReportBuilder>
+   public interface IReportBuilderRepository : IBuilderRepository<IReportBuilder>
    {
    }
 
-   public class ReportBuilderRepository :BuilderRepository<IReportBuilder>, IReportBuilderRepository
+   public class ReportBuilderRepository : BuilderRepository<IReportBuilder>, IReportBuilderRepository
    {
       public ReportBuilderRepository(IContainer container) : base(container, typeof(IReportBuilder<>))
       {
