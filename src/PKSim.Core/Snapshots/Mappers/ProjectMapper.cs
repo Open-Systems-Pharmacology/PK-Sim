69781e1f856d266f95269dd52546ba3cce54f8af
Michael Sevestre <michael@design2code.ca>
2017-09-08 15:49:58 -0400

    Start project mapping to snapshot

--- src/PKSim.Core/Snapshots/Mappers/ProjectMapper.cs
+++ src/PKSim.Core/Snapshots/Mappers/ProjectMapper.cs
@@ -1,19 +1,47 @@
-﻿using SnapshotProject  = PKSim.Core.Snapshots.Project;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OSPSuite.Utility.Extensions;
+using PKSim.Core.Model;
+using SnapshotProject = PKSim.Core.Snapshots.Project;
 using ModelProject = PKSim.Core.Model.PKSimProject;
-
 
 namespace PKSim.Core.Snapshots.Mappers
 {
    public class ProjectMapper : ObjectBaseSnapshotMapperBase<ModelProject, SnapshotProject>
    {
+      private readonly IExecutionContext _executionContext;
+
+      public ProjectMapper(IExecutionContext executionContext)
+      {
+         _executionContext = executionContext;
+      }
+
       public override SnapshotProject MapToSnapshot(ModelProject project)
       {
-         throw new System.NotImplementedException();
+         return SnapshotFrom(project, snapshot =>
+         {
+            snapshot.Individuals = mapBuildingBlocks<Individual>(project.All<Model.Individual>());
+            snapshot.Compounds = mapBuildingBlocks<Compound>(project.All<Model.Compound>());
+         });
+      }
+
+      private List<T> mapBuildingBlocks<T>(IReadOnlyCollection<IPKSimBuildingBlock> buildingBlocks)
+      {
+         //required to load the snapshot mapper via execution context to avoid circular references
+         var snapshotMapper = _executionContext.Resolve<ISnapshotMapper>();
+         return buildingBlocks.Select(bb => mapBuildingBlockToSnapshot<T>(bb, snapshotMapper)).ToList();
+      }
+
+      private T mapBuildingBlockToSnapshot<T>(IPKSimBuildingBlock buildingBlock, ISnapshotMapper snapshotMapper)
+      {
+         _executionContext.Load(buildingBlock);
+         return snapshotMapper.MapToSnapshot(buildingBlock).DowncastTo<T>();
       }
 
       public override ModelProject MapToModel(SnapshotProject snapshot)
       {
-         throw new System.NotImplementedException();
+         throw new NotImplementedException();
       }
    }
 }
---
+++ tests/PKSim.Tests/Core/ProjectMapperSpecs.cs
@@ -0,0 +1,59 @@
+﻿using FakeItEasy;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using PKSim.Core.Model;
+using PKSim.Core.Snapshots.Mappers;
+using Project = PKSim.Core.Snapshots.Project;
+
+namespace PKSim.Core
+{
+   public abstract class concern_for_ProjectMapper : ContextSpecification<ProjectMapper>
+   {
+      protected PKSimProject _project;
+      private Individual _individual;
+      private Compound _compound;
+      private IndividualSimulation _simulation;
+      protected Project _snapshot;
+      private ISnapshotMapper _snapshotMapper;
+      protected Snapshots.Compound _compoundSnapshot;
+      protected Snapshots.Individual _individualSnapshot;
+      private IExecutionContext _executionContext;
+
+      protected override void Context()
+      {
+         _snapshotMapper = A.Fake<ISnapshotMapper>();
+         _executionContext = A.Fake<IExecutionContext>();
+         sut = new ProjectMapper(_executionContext);
+         A.CallTo(() => _executionContext.Resolve<ISnapshotMapper>()).Returns(_snapshotMapper);
+         _individual = new Individual().WithName("IND");
+         _compound = new Compound().WithName("COMP");
+//         _simulation = new IndividualSimulation().WithName("IND_SIM");
+
+         _project = new PKSimProject();
+         _project.AddBuildingBlock(_individual);
+         _project.AddBuildingBlock(_compound);
+         //         _project.AddBuildingBlock(_simulation);
+
+         _compoundSnapshot = new Snapshots.Compound();
+         _individualSnapshot = new Snapshots.Individual();
+         A.CallTo(() => _snapshotMapper.MapToSnapshot(_compound)).Returns(_compoundSnapshot);
+         A.CallTo(() => _snapshotMapper.MapToSnapshot(_individual)).Returns(_individualSnapshot);
+      }
+   }
+
+   public class When_exporting_a_project_to_snapshot : concern_for_ProjectMapper
+   {
+      protected override void Because()
+      {
+         _snapshot = sut.MapToSnapshot(_project);
+      }
+
+      [Observation]
+      public void should_retrieve_the_snapshot_for_all_underlying_building_blocks()
+      {
+         _snapshot.Compounds.ShouldContain(_compoundSnapshot);
+         _snapshot.Individuals.ShouldContain(_individualSnapshot);
+      }
+   }
+}
