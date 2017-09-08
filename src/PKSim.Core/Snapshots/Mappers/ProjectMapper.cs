c8a1eb18b82cea07e42727d10d29f5f33db82f4d
Michael Sevestre <michael@design2code.ca>
2017-09-08 08:56:26 -0400

    Fixes #318 define snapshot project structure (#321)

---
+++ src/PKSim.Core/Snapshots/Mappers/ProjectMapper.cs
@@ -0,0 +1,19 @@
+﻿using SnapshotProject  = PKSim.Core.Snapshots.Project;
+using ModelProject = PKSim.Core.Model.PKSimProject;
+
+
+namespace PKSim.Core.Snapshots.Mappers
+{
+   public class ProjectMapper : ObjectBaseSnapshotMapperBase<ModelProject, SnapshotProject>
+   {
+      public override SnapshotProject MapToSnapshot(ModelProject project)
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public override ModelProject MapToModel(SnapshotProject snapshot)
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
---
+++ src/PKSim.Core/Snapshots/Project.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PKSim.Core.Snapshots
+{
+   public class Project : SnapshotBase
+   {
+      public List<Individual> Individuals { get; set; } = new List<Individual>();
+      public List<Population> Populations { get; set; } = new List<Population>();
+      public List<Compound> Compounds { get; set; } = new List<Compound>();
+      public List<Formulation> Formulations { get; set; } = new List<Formulation>();
+      public List<Protocol> Protocols { get; set; } = new List<Protocol>();
+      public List<Event> Events { get; set; } = new List<Event>();
+   }
+}
