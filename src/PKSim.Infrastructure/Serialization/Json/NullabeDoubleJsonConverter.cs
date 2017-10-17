d494bdd08ab64fe32617c121a8d06d6751ec9e6c
Michael Sevestre <michael@design2code.ca>
2017-10-17 12:59:31 -0400

    Fixes #385 Parameter Export to snapshot. Exported value should be rounded

---
+++ src/PKSim.Infrastructure/Serialization/Json/NullabeDoubleJsonConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PKSim.Infrastructure.Serialization.Json
+{
+   public class NullabeDoubleJsonConverter : JsonConverter
+   {
+      private const int DOUBLE_PRECISION = 10;
+
+      public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+      {
+         var d = (double?) value;
+         if (!d.HasValue)
+            return;
+
+         var rounded = Math.Round(d.Value, DOUBLE_PRECISION);
+         writer.WriteValue(rounded);
+      }
+
+      public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+      {
+         //this will never be called for nullable as double reader will take precedence
+         return 0;
+      }
+
+      public override bool CanRead => false;
+
+      public override bool CanConvert(Type objectType)
+      {
+         return objectType == typeof(double?);
+      }
+   }
+}
---
+++ tests/PKSim.Tests/Infrastructure/SnapshotSerializerSpecs.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Utility;
+using PKSim.Core.Snapshots;
+using PKSim.Core.Snapshots.Services;
+using PKSim.Infrastructure.Serialization.Json;
+
+namespace PKSim.Infrastructure
+{
+   public abstract class concern_for_SnapshotSerializer : ContextSpecificationAsync<ISnapshotSerializer>
+   {
+      protected Parameter _parameter;
+      protected string _fileName;
+      protected Parameter _deserialiedParameter;
+
+      public override async Task GlobalContext()
+      {
+         await base.GlobalContext();
+         _fileName = FileHelper.GenerateTemporaryFileName();
+      }
+
+      protected override Task Context()
+      {
+         _parameter = new Parameter();
+         sut = new SnapshotSerializer();
+         return _completed;
+      }
+
+      protected override async Task Because()
+      {
+         await sut.Serialize(_parameter, _fileName);
+         _deserialiedParameter = (await sut.DeserializeAsArray(_fileName, typeof(Parameter))).Cast<Parameter>().First();
+      }
+
+      public override async Task GlobalCleanup()
+      {
+         await base.GlobalCleanup();
+         FileHelper.DeleteFile(_fileName);
+      }
+   }
+
+   public class When_serializing_a_parameter_with_a_value_not_set : concern_for_SnapshotSerializer
+   {
+      protected override async Task Context()
+      {
+         await base.Context();
+         _parameter.Value = null;
+      }
+
+      [Observation]
+      public void should_deserialize_the_parmaeter_with_a_value_also_not_set()
+      {
+         _deserialiedParameter.Value.ShouldBeNull();
+      }
+   }
+
+   public class When_serialziing_a_parameter_with_a_value_set_with_a_mantis_of_length_less_than_the_precision : concern_for_SnapshotSerializer
+   {
+      protected override async Task Context()
+      {
+         await base.Context();
+         _parameter.Value = 1.123456789;
+      }
+
+      [Observation]
+      public void should_not_round_the_number()
+      {
+         _deserialiedParameter.Value.ShouldBeEqualTo(_parameter.Value);
+      }
+   }
+
+   public class When_serializing_a_parameter_whose_value_was_tainted_with_numerical_noise : concern_for_SnapshotSerializer
+   {
+      protected override async Task Context()
+      {
+         await base.Context();
+         _parameter.Value = 449.9999999999991;
+      }
+
+      [Observation]
+      public void should_round_the_value()
+      {
+         _deserialiedParameter.Value.ShouldBeEqualTo(450);
+      }
+   }
+
+   public class When_serializing_an_array_of_objects_to_json : concern_for_SnapshotSerializer
+   {
+      private IEnumerable<Parameter> _deserialiedParameters;
+
+      protected override async Task Because()
+      {
+         await sut.Serialize(new[] {_parameter, _parameter}, _fileName);
+         _deserialiedParameters = (await sut.DeserializeAsArray(_fileName, typeof(Parameter))).Cast<Parameter>();
+      }
+
+      [Observation]
+      public void should_be_able_to_retrievee_an_array_of_deserialized_objects()
+      {
+         _deserialiedParameters.Count().ShouldBeEqualTo(2);
+      }
+   }
+}
