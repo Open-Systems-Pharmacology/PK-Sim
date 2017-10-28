8b90ed75ad23b0ca3a3af7b401ebc13b88cff9b8
Yuri05 <juri.solodenko@bayer.com>
2017-10-29 01:29:43 +0200

    In progress: #368 Integrate pregnant population

---
+++ src/PKSim.Core/Model/PopulationAgeSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKSim.Core.Model
+{
+    public class PopulationAgeSettings
+    {
+        public string Population { get; set; }
+        public double DefaultAge { get; set; }
+        public double MinAge { get; set; }
+        public double MaxAge { get; set; }
+        public string DefaultAgeUnit { get; set; }
+    }
+}
---
+++ src/PKSim.Core/Repositories/IPopulationAgeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OSPSuite.Utility.Collections;
+using OSPSuite.Utility.Extensions;
+using PKSim.Core.Model;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Services;
+
+namespace PKSim.Core.Repositories
+{
+    public interface IPopulationAgeRepository : IStartableRepository<PopulationAgeSettings>
+    {
+        PopulationAgeSettings PopulationAgeSettingsFrom(string population);
+    }
+}
