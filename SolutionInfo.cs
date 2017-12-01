105e310e3e48c412b1515601cb3667508921be55
Michael Sevestre <michael@design2code.ca>
2017-12-01 13:12:18 -0500

    Bump to version 7.3.0

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("PK-Sim")]
 [assembly: AssemblyCopyright("Copyright © 2002-2017 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("7.2.0.0")]
-[assembly: AssemblyFileVersion("7.2.0.0")]
+[assembly: AssemblyVersion("7.3.0.0")]
+[assembly: AssemblyFileVersion("7.3.0.0")]
 [assembly: AssemblyInformationalVersion("")]
 
 [assembly: InternalsVisibleTo("PKSim.Tests")]
