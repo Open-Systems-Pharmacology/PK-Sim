780aacac519a1b08a5e654041614c6892119c8d3
Michael Sevestre <michael@design2code.ca>
2018-05-03 11:20:46 -0400

    Fixes #646 version information missing in CLI (#671)

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Open Systems Pharmacology")]
 [assembly: AssemblyProduct("PK-Sim")]
-[assembly: AssemblyCopyright("Copyright © 2002-2017 - Open Systems Pharmacology Community")]
+[assembly: AssemblyCopyright("Copyright © 2002-2018 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("7.3.0.0")]
 [assembly: AssemblyFileVersion("7.3.0.0")]
-[assembly: AssemblyInformationalVersion("")]
+[assembly: AssemblyInformationalVersion("7.3.0.0")]
 
 [assembly: InternalsVisibleTo("PKSim.Tests")]
 [assembly: InternalsVisibleTo("PKSim.UI.Tests")]
