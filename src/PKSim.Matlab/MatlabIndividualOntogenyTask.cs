f21ecac7682c179dcc1de2c682065527abede268
Michael Sevestre <michael@design2code.ca>
2018-05-14 11:33:23 -0400

    Fixes issues with missing dependencies when running matlab code

---
+++ src/PKSim.Matlab/MatlabIndividualOntogenyTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OSPSuite.Core.Commands.Core;
+using PKSim.Core;
+using PKSim.Core.Commands;
+using PKSim.Core.Model;
+using PKSim.Core.Services;
+
+namespace PKSim.Matlab
+{
+   public class MatlabIndividualOntogenyTask : IOntogenyTask<Individual>
+   {
+      private readonly IExecutionContext _executionContext;
+
+      public MatlabIndividualOntogenyTask(IExecutionContext executionContext)
+      {
+         _executionContext = executionContext;
+      }
+
+      public ICommand SetOntogenyForMolecule(IndividualMolecule molecule, Ontogeny ontogeny, Individual individual)
+      {
+         return new SetOntogenyInMoleculeForIndividualCommand(molecule, ontogeny, individual, _executionContext).Run(_executionContext);
+      }
+
+      public void ShowOntogenyData(Ontogeny ontogeny)
+      {
+         throw new NotSupportedException();
+      }
+
+      public ICommand LoadOntogenyForMolecule(IndividualMolecule molecule, Individual simulationSubject)
+      {
+         throw new NotSupportedException();
+      }
+   }
+}
