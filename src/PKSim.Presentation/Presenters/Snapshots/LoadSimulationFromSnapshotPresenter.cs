00a16ecc6a365c2cd8739498ed0e754c6934d09e
Michael Sevestre <michael@design2code.ca>
2018-05-25 15:37:30 -0400

    Ensure that load simulation also used own presenter

---
+++ src/PKSim.Presentation/Presenters/Snapshots/LoadSimulationFromSnapshotPresenter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OSPSuite.Core.Domain.Services;
+using OSPSuite.Core.Services;
+using OSPSuite.Presentation.Presenters;
+using OSPSuite.Utility.Events;
+using PKSim.Core.Model;
+using PKSim.Core.Services;
+using PKSim.Core.Snapshots.Mappers;
+using PKSim.Core.Snapshots.Services;
+using PKSim.Presentation.Views.Snapshots;
+
+namespace PKSim.Presentation.Presenters.Snapshots
+{
+   public interface ILoadSimulationFromSnapshotPresenter : ILoadFromSnapshotPresenter<Simulation>
+   {
+      /// <summary>
+      ///    Simulation loaded from selected snapshot file. It is null if the user cancels the action or if the file was not
+      ///    loaded properly
+      /// </summary>
+      Simulation LoadSimulation();
+   }
+
+   public class LoadSimulationFromSnapshotPresenter : LoadFromSnapshotPresenter<Simulation>, ILoadSimulationFromSnapshotPresenter
+   {
+      private readonly SimulationMapper _simulationMapper;
+      private readonly IPKSimProjectRetriever _projectRetriever;
+
+      public LoadSimulationFromSnapshotPresenter(
+         ILoadFromSnapshotView view,
+         ILogPresenter logPresenter,
+         ISnapshotTask snapshotTask,
+         IDialogCreator dialogCreator,
+         IObjectTypeResolver objectTypeResolver,
+         ILogger logger,
+         IEventPublisher eventPublisher,
+         SimulationMapper simulationMapper,
+         IPKSimProjectRetriever projectRetriever
+      ) : base(view, logPresenter, snapshotTask, dialogCreator, objectTypeResolver, logger, eventPublisher)
+      {
+         _simulationMapper = simulationMapper;
+         _projectRetriever = projectRetriever;
+      }
+
+      public Simulation LoadSimulation()
+      {
+         var models = LoadModelFromSnapshot();
+         return models?.FirstOrDefault();
+      }
+
+      protected override async Task<IEnumerable<Simulation>> LoadModelAsync(string snapshotFile)
+      {
+         var snapshots = await _snapshotTask.LoadSnapshot<PKSim.Core.Snapshots.Simulation>();
+         var tasks = snapshots.Select(x => _simulationMapper.MapToModel(x, _projectRetriever.Current));
+         return await Task.WhenAll(tasks);
+      }
+   }
+}
