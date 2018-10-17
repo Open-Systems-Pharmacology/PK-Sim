95f2119b75d0bfe100bfacab8f6cf7c8342614cc
Michael Sevestre <michael@design2code.ca>
2018-10-17 11:06:58 -0400

    Fixes #309 ontogeny factor visible in simulation (#978)

--- tests/PKSim.Tests/ProjectConverter/v7_4/Converter730To740Specs.cs
+++ tests/PKSim.Tests/ProjectConverter/v7_4/Converter730To740Specs.cs
@@ -4,6 +4,7 @@ using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
 using OSPSuite.Core.Domain;
 using OSPSuite.Utility.Extensions;
+using PKSim.Core;
 using PKSim.Core.Model;
 using PKSim.Infrastructure.ProjectConverter;
 using PKSim.Infrastructure.ProjectConverter.v7_4;
@@ -14,13 +15,16 @@ namespace PKSim.ProjectConverter.v7_4
    public class When_converting_the_simple_project_730_project : ContextWithLoadedProject<Converter730To740>
    {
       private List<Simulation> _allSimulations;
+      private List<Individual> _allIndividuals;
 
       public override void GlobalContext()
       {
          base.GlobalContext();
          LoadProject("SimpleProject_730");
          _allSimulations = All<Simulation>().ToList();
+         _allIndividuals = All<Individual>().ToList();
          _allSimulations.Each(Load);
+         _allIndividuals.Each(Load);
       }
 
       [Observation]
@@ -38,9 +42,49 @@ namespace PKSim.ProjectConverter.v7_4
          allParameters.Each(p =>
          {
             p.Visible.ShouldBeTrue();
-            p.Info.ReadOnly.ShouldBeFalse();
+            p.Editable.ShouldBeTrue();
             p.Sequence.ShouldBeGreaterThan(10);
          });
+      }
+
+      [Observation]
+      public void should_have_updated_the_ontogeny_factor_parameters_in_all_individual_to_be_visible_editable_and_default_value_set_to_current_value()
+      {
+         _allIndividuals.Each(verifyIndividual);
+         _allSimulations.Select(s => s.BuildingBlock<Individual>()).Each(verifyIndividual);
+      }
+
+
+      [Observation]
+      public void should_have_updated_the_plasma_protein_ontogeny_factor_in_the_simulations()
+      {
+         _allSimulations.Each(s => verifyPlasmaProteinOntogenyFactor(s.Model.Root.Container(Constants.ORGANISM)));
+         _allSimulations.Select(s => s.BuildingBlock<Individual>()).Each(verifyIndividual);
+      }
+
+      private void verifyIndividual(Individual individual)
+      {
+         verifyPlasmaProteinOntogenyFactor(individual.Organism);
+         individual.AllDefinedMolecules().Each(verifyOntogenyInMolecule);
+      }
+
+      private void verifyPlasmaProteinOntogenyFactor(IContainer container)
+      {
+         verifyOntogenyParameter(container.Parameter(CoreConstants.Parameters.ONTOGENY_FACTOR_ALBUMIN));
+         verifyOntogenyParameter(container.Parameter(CoreConstants.Parameters.ONTOGENY_FACTOR_AGP));
+      }
+
+      private void verifyOntogenyInMolecule(IndividualMolecule molecule)
+      {
+         verifyOntogenyParameter(molecule.OntogenyFactorParameter);
+         verifyOntogenyParameter(molecule.OntogenyFactorGIParameter);
+      }
+
+      private void verifyOntogenyParameter(IParameter parameter)
+      {
+         parameter.Visible.ShouldBeTrue();
+         parameter.Editable.ShouldBeTrue();
+         parameter.DefaultValue.ShouldBeEqualTo(parameter.Value);
       }
    }
 }
