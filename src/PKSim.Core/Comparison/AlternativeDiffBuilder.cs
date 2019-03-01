073688c8bf3eb5ae3f11ddfb5e3e2a42560c37fa
Michael Sevestre <michael@design2code.ca>
2019-03-01 14:33:05 -0500

    1027 default diff (#1121)
    * Add diff builder for Parameter alternative
    
    * Fixes #1027 default and calculation method diff

---
+++ src/PKSim.Core/Comparison/AlternativeDiffBuilder.cs
@@ -0,0 +1,22 @@
+﻿using OSPSuite.Core.Comparison;
+using PKSim.Assets;
+using PKSim.Core.Model;
+
+namespace PKSim.Core.Comparison
+{
+   public class AlternativeDiffBuilder : DiffBuilder<ParameterAlternative>
+   {
+      private readonly ContainerDiffBuilder _containerDiffBuilder;
+
+      public AlternativeDiffBuilder(ContainerDiffBuilder containerDiffBuilder)
+      {
+         _containerDiffBuilder = containerDiffBuilder;
+      }
+
+      public override void Compare(IComparison<ParameterAlternative> comparison)
+      {
+         _containerDiffBuilder.Compare(comparison);
+         CompareStringValues(x => x.IsDefault.ToString(), PKSimConstants.UI.IsDefault, comparison);
+      }
+   }
+}
