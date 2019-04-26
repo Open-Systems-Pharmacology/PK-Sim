ad71e2f5a90c6956ee44a090c3c7df998d2759be
Michael Sevestre <michael@design2code.ca>
2019-04-26 12:50:48 -0400

    Fixes #1213 crash exporting

---
+++ tests/PKSim.Tests/Infrastructure/PKSimProjectRetrieverSpecs.cs
@@ -0,0 +1,34 @@
+﻿using FakeItEasy;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using PKSim.Core;
+using PKSim.Core.Services;
+using PKSim.Infrastructure.Services;
+
+namespace PKSim.Infrastructure
+{
+   public abstract class concern_for_PKSimProjectRetriever : ContextSpecification<IPKSimProjectRetriever>
+   {
+      protected ICoreWorkspace _workspace;
+
+      protected override void Context()
+      {
+         _workspace= A.Fake<ICoreWorkspace>();  
+         sut = new PKSimProjectRetriever(_workspace);
+      }
+   }
+
+   public class When_retrieving_the_project_name_when_the_project_is_not_defined : concern_for_PKSimProjectRetriever
+   {
+      protected override void Because()
+      {
+         _workspace.Project = null;
+      }
+
+      [Observation]
+      public void should_return_an_empty_string()
+      {
+         sut.ProjectName.ShouldBeEqualTo(string.Empty);
+      }
+   }
+}	
