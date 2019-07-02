739a0d73c2832762d4879bf5c0004bf0612b662b
Michael Sevestre <michael@design2code.ca>
2019-07-02 16:21:53 -0400

    1286 cli crash (#1287)
    * Fixes #1286 cli crash
    
    * Also add tests

--- src/PKSim.CLI.Core/CLIRegister.cs
+++ src/PKSim.CLI.Core/CLIRegister.cs
@@ -1,4 +1,5 @@
 ﻿using OSPSuite.Utility.Container;
+using PKSim.CLI.Core.RunOptions;
 using PKSim.CLI.Core.Services;
 using PKSim.Core;
 
@@ -15,9 +16,12 @@ namespace PKSim.CLI.Core
             //Register services
             x.IncludeNamespaceContainingType<SnapshotRunner>();
 
- 
+
             x.WithConvention<PKSimRegistrationConvention>();
          });
+
+         //special registration that does not follow conventions
+         container.Register<IBatchRunner<ExportRunOptions>, ExportSimulationRunner>();
       }
    }
 }
