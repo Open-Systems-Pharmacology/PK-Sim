0c7cc5f85f856c6c10da8392c350f86b1f15ebb8
Michael Sevestre <michael@design2code.ca>
2019-11-15 16:40:29 -0500

    Update code to work with new tags concept

--- src/PKSim.Infrastructure/ORM/FlatObjects/FlatCalculationMethodRateDescriptorConditions.cs
+++ src/PKSim.Infrastructure/ORM/FlatObjects/FlatCalculationMethodRateDescriptorConditions.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace PKSim.Infrastructure.ORM.FlatObjects
+﻿namespace PKSim.Infrastructure.ORM.FlatObjects
 {
-   public class FlatCalculationMethodRateDescriptorConditions : FlatDescriptorConditionBase
+   public class FlatCalculationMethodRateDescriptorConditions 
    {
+      public string Tag { get; set; }
       public string CalculationMethod { get; set; }
       public string Rate { get; set; }
+      public CriteriaCondition Condition { get; set; }
    }
 }
