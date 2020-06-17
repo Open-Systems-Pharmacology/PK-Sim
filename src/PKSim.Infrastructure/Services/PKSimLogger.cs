b10cc86591ba3d1693698f399066fb60089548e4
Abdel Rodríguez <abdelr1982@gmail.com>
2020-06-17 08:55:06 +0200

    Update PKSimLogger.cs

--- src/PKSim.Infrastructure/Services/PKSimLogger.cs
+++ src/PKSim.Infrastructure/Services/PKSimLogger.cs
@@ -7,73 +7,61 @@ using System.Collections.Generic;
 using System.Linq;
 using PKSim.Infrastructure.Extensions;
 using ILogger = OSPSuite.Core.Services.ILogger;
+using System.Collections.Concurrent;
 
 namespace PKSim.Infrastructure.Services
 {
   public class PKSimLogger : ILogger
   {
     private const string DEFAULT_LOGGER_CATEGORY = "PK-Sim";
-    private ILoggerFactory _loggerFactory;
+    private ConcurrentDictionary<string, Microsoft.Extensions.Logging.ILogger> _logger;
     private List<Func<ILoggingBuilder, ILoggingBuilder>> _loggingBuilderConfigurations = new List<Func<ILoggingBuilder, ILoggingBuilder>>() { builder => builder };
-    private List<ILoggerProvider> _loggerProviders = new List<ILoggerProvider>();
-
-    public PKSimLogger()
-    {
-      _loggerFactory = null;
-    }
 
     public void AddToLog(string message, LogLevel logLevel, string categoryName)
     {
-      if (_loggerFactory != null)// Maybe throw an error if null, i.e. not configured
+      var logger = _logger.GetOrAdd(categoryName, (_) => SetupFactory((string.IsNullOrEmpty(categoryName) ? DEFAULT_LOGGER_CATEGORY : categoryName)));
+      switch (logLevel)
       {
-        var logger = _loggerFactory.CreateLogger(string.IsNullOrEmpty(categoryName) ? DEFAULT_LOGGER_CATEGORY : categoryName);
-        switch (logLevel)
-        {
-          case LogLevel.Trace:
-            logger.LogTrace(message);
-            break;
-          case LogLevel.Debug:
-            logger.LogDebug(message);
-            break;
-          case LogLevel.Information:
-            logger.LogInformation(message);
-            break;
-          case LogLevel.Warning:
-            logger.LogWarning(message);
-            break;
-          case LogLevel.Error:
-            logger.LogError(message);
-            break;
-          case LogLevel.Critical:
-            logger.LogCritical(message);
-            break;
-        }
+        case LogLevel.Trace:
+          logger.LogTrace(message);
+          break;
+        case LogLevel.Debug:
+          logger.LogDebug(message);
+          break;
+        case LogLevel.Information:
+          logger.LogInformation(message);
+          break;
+        case LogLevel.Warning:
+          logger.LogWarning(message);
+          break;
+        case LogLevel.Error:
+          logger.LogError(message);
+          break;
+        case LogLevel.Critical:
+          logger.LogCritical(message);
+          break;
       }
     }
 
     public PKSimLogger AddLoggingBuilderConfiguration(Func<ILoggingBuilder, ILoggingBuilder> configuration)
     {
       _loggingBuilderConfigurations.Add(configuration);
-      SetupFactory();
       return this;
     }
 
-    public PKSimLogger AddLoggerProvider(ILoggerProvider loggerprovider)
+    private Microsoft.Extensions.Logging.ILogger SetupFactory(string categoryName)
     {
-      _loggerProviders.Add(loggerprovider);
-      SetupFactory();
-      return this;
-    }
-
-    private void SetupFactory()
-    {
-      _loggerFactory = LoggerFactory.Create(
+      Microsoft.Extensions.Logging.ILogger result;
+      using (var loggerFactory = LoggerFactory.Create(
           builder =>
           _loggingBuilderConfigurations.Aggregate(
               (f1, f2) => config => f1.Compose(f2, config)
           ).Invoke(builder)
-      );
-      _loggerProviders.ForEach(provider => _loggerFactory.AddProvider(provider));
+      ))
+      {
+        result = loggerFactory.CreateLogger(categoryName);
+      };
+      return result;
     }
   }
 }
