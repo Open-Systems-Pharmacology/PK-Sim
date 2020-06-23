ad45c6c6ad5d3cc6150714e01f410ff23ae75d0a
Michael Sevestre <michael@design2code.ca>
2020-06-23 10:14:10 -0400

    FIxes #811 update to v10

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Open Systems Pharmacology")]
 [assembly: AssemblyProduct("PK-Sim")]
-[assembly: AssemblyCopyright("Copyright © 2002-2019 - Open Systems Pharmacology Community")]
+[assembly: AssemblyCopyright("Copyright © 2002-2020 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("PKSim.Tests")]
