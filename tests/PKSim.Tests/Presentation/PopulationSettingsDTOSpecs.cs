2b5c362d152a99f689c2f2ea585d7fb32487e3bd
Abdel Rodríguez <abdelr1982@gmail.com>
2020-07-06 16:23:17 +0200

    Fixes #1492
    * Update PopulationSettingsDTO.cs
    
    * Create PopulationSettingsDTOSpecs.cs
    
    * Using IsValid to simplify the call
    
    * Using BDD for asserting

---
+++ tests/PKSim.Tests/Presentation/PopulationSettingsDTOSpecs.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Utility.Validation;
+using PKSim.Presentation.DTO.Populations;
+
+namespace PKSim.Presentation
+{
+   public class concern_for_PopulationSettingsDTO : ContextSpecification<PopulationSettingsDTO>
+   {
+      protected override void Context()
+      {
+         sut = new PopulationSettingsDTO();
+         sut.NumberOfIndividuals = 5;
+         sut.ProportionOfFemales = 50;
+      }
+
+      [TestCase(50, true)]
+      [TestCase(101, false)]
+      [TestCase(-1, false)]
+      public void FemaleRate_should_be_properly_bounded(int femaleRate, bool expectation)
+      {
+         sut.ProportionOfFemales = femaleRate;
+         sut.IsValid().ShouldBeEqualTo(expectation);
+      }
+   }
+}
