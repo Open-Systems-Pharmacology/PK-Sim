f7a39cf1f4b5c48555ad12efd16c911da708a9ca
Michael Sevestre <michael@design2code.ca>
2021-03-30 14:56:53 -0400

    WIP #1647 make code compile again and ensure that we can load a compound without a molecule

--- src/PKSim.CLI.Core/MinimalImplementations/CLIDataImporter.cs
+++ src/PKSim.CLI.Core/MinimalImplementations/CLIDataImporter.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using OSPSuite.Core.Domain.Data;
 using OSPSuite.Infrastructure.Import.Core;
 using OSPSuite.Infrastructure.Import.Services;
-using OSPSuite.Utility.Collections;
 using ImporterConfiguration = OSPSuite.Core.Import.ImporterConfiguration;
 
 namespace PKSim.CLI.Core.MinimalImplementations
@@ -12,10 +11,20 @@ namespace PKSim.CLI.Core.MinimalImplementations
    {
       public (IReadOnlyList<DataRepository> DataRepositories, ImporterConfiguration Configuration) ImportDataSets(IReadOnlyList<MetaDataCategory> metaDataCategories, IReadOnlyList<ColumnInfo> columnInfos, DataImporterSettings dataImporterSettings)
       {
+         throw new NotImplementedException();
+      }
+
+      public IReadOnlyList<DataRepository> ImportFromConfiguration(ImporterConfiguration configuration, IReadOnlyList<MetaDataCategory> metaDataCategories, IReadOnlyList<ColumnInfo> columnInfos, DataImporterSettings dataImporterSettings)
+      {
          throw new System.NotImplementedException();
       }
 
-      public IReadOnlyList<DataRepository> ImportFromConfiguration(ImporterConfiguration configuration, bool promptForConfirmation, IReadOnlyList<MetaDataCategory> metaDataCategories, IReadOnlyList<ColumnInfo> columnInfos, DataImporterSettings dataImporterSettings)
+      public ReloadDataSets CalculateReloadDataSetsFromConfiguration(IReadOnlyList<DataRepository> dataSetsToImport, IReadOnlyList<DataRepository> existingDataSets)
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public IList<MetaDataCategory> DefaultMetaDataCategories()
       {
          throw new System.NotImplementedException();
       }
