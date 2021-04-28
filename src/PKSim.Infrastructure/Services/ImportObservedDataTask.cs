d80379b5919569e8d266a2184b2052316f2739fc
Abdel Rodríguez <abdelr1982@gmail.com>
2021-04-28 21:24:57 +0200

    Restoring original way to populate the organs

--- src/PKSim.Infrastructure/Services/ImportObservedDataTask.cs
+++ src/PKSim.Infrastructure/Services/ImportObservedDataTask.cs
@@ -481,7 +481,8 @@ namespace PKSim.Infrastructure.Services
 
          var defaultIndividual = _defaultIndividualRetriever.DefaultIndividual();
          var organism = defaultIndividual.Organism;
-         foreach (var organ in organism.OrgansByType(OrganType.VascularSystem | OrganType.Tissue | OrganType.Lumen | OrganType.Unknown))
+         addInfoToCategory(nameCategory, organism.Organ(CoreConstants.Organ.PERIPHERAL_VENOUS_BLOOD));
+         foreach (var organ in organism.OrgansByType(OrganType.VascularSystem | OrganType.Tissue | OrganType.Lumen))
          {
             nameCategory.ListOfValues.Add(organ.Name, organ.Name);
          }
