c2fdc41a8e432c7d803a953f16071f4e8de9ed06
Michael Sevestre <michael@design2code.ca>
2021-05-04 12:55:10 -0400

    Fixes #1724 expression profile lost after scaling (#1745)
    * Fixes #1724 expression profile lost after scaling
    
    * Fixes #1724

--- src/PKSim.Core/Services/IndividualExpressionsUpdater.cs
+++ src/PKSim.Core/Services/IndividualExpressionsUpdater.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Linq;
 using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Services;
 using OSPSuite.Utility.Extensions;
 using PKSim.Core.Model;
 
@@ -13,30 +15,75 @@ namespace PKSim.Core.Services
    public class IndividualExpressionsUpdater : IIndividualExpressionsUpdater
    {
       private readonly IOntogenyTask<Individual> _ontogenyTask;
+      private readonly IMoleculeExpressionTask<Individual> _moleculeExpressionTask;
+      private readonly IEntityPathResolver _entityPathResolver;
       private readonly ICloner _cloner;
+      private readonly IParameterUpdater _parameterUpdater;
 
-      public IndividualExpressionsUpdater(IOntogenyTask<Individual> ontogenyTask, ICloner cloner)
+      public IndividualExpressionsUpdater(
+         IOntogenyTask<Individual> ontogenyTask, 
+         IMoleculeExpressionTask<Individual> moleculeExpressionTask, 
+         IEntityPathResolver entityPathResolver,
+         ICloner cloner, 
+         IParameterUpdater parameterUpdater)
       {
          _ontogenyTask = ontogenyTask;
+         _moleculeExpressionTask = moleculeExpressionTask;
+         _entityPathResolver = entityPathResolver;
          _cloner = cloner;
+         _parameterUpdater = parameterUpdater;
       }
 
       public void Update(Individual sourceIndividual, Individual targetIndividual)
       {
-         foreach (var molecule in sourceIndividual.AllMolecules())
+         foreach (var sourceMolecule in sourceIndividual.AllMolecules())
          {
-            var newMolecule = _cloner.Clone(molecule);
-            targetIndividual.AddMolecule(newMolecule);
+            //Add molecule to the target individual based on the new individual
+            _moleculeExpressionTask.AddMoleculeTo(targetIndividual, sourceMolecule);
 
-            //Make sure parameters that user defined parameters are reset to default to ensure proper scaling
-            resetMoleculeParametersToDefault(newMolecule);
+            var targetMolecule = targetIndividual.MoleculeByName(sourceMolecule.Name);
+
+            //ensure that all global properties are updated
+            targetMolecule.UpdatePropertiesFrom(sourceMolecule, _cloner);
+
+            //Update all parameters from source to target
+            updatePathCache(sourceIndividual.AllMoleculeParametersFor(sourceMolecule), targetIndividual.AllMoleculeParametersFor(targetMolecule));
+
+            //Update all containers from source to target
+            updatePathCache(sourceIndividual.AllMoleculeContainersFor(sourceMolecule), targetIndividual.AllMoleculeContainersFor(targetMolecule));
+
+            //Make sure global parameters are reset to default to ensure proper scaling (they will be part of the scaling algorithm)
+            resetGlobalMoleculeParametersToDefault(targetMolecule);
 
             //we have to reset the ontogeny for the molecule based on the target individual properties
-            _ontogenyTask.SetOntogenyForMolecule(newMolecule, newMolecule.Ontogeny, targetIndividual);
+            _ontogenyTask.SetOntogenyForMolecule(targetMolecule, targetMolecule.Ontogeny, targetIndividual);
          }
       }
 
-      private void resetMoleculeParametersToDefault(IndividualMolecule molecule)
+      private void updatePathCache<T>(IEnumerable<T> sourceEntities, IEnumerable<T> targetEntities) where T: class, IEntity
+      {
+         var sourcePathCache = new PathCache<T>(_entityPathResolver).For(sourceEntities);
+         var targetPathCache = new PathCache<T>(_entityPathResolver).For(targetEntities);
+
+         sourcePathCache.KeyValues.Each(kv => updateEntities(kv.Value, targetPathCache[kv.Key]));
+      }
+
+      private void updateEntities<T>(T sourceEntity, T targetEntity) where T : class, IEntity
+      {
+         //this should never happen
+         if (targetEntity == null)
+            return;
+
+         targetEntity.UpdatePropertiesFrom(sourceEntity, _cloner);   
+
+         //for Parameters, we need to also update the values for constant parameters updated by the user. This is not done by default 
+         if (!(targetEntity is IParameter targetParam) || !(sourceEntity is IParameter sourceParam))
+            return;
+
+         _parameterUpdater.UpdateValue(sourceParam, targetParam);
+      }
+
+      private void resetGlobalMoleculeParametersToDefault(IndividualMolecule molecule)
       {
          molecule.AllParameters()
             .Where(x => x.ValueDiffersFromDefault())
