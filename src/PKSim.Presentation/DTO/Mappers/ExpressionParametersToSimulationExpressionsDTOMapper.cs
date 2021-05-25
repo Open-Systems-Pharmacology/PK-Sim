1cfc781ecc0299c30165328b871b0f98e803d0ce
Michael Sevestre <michael@design2code.ca>
2021-05-25 15:11:14 -0400

    Fixes #1765 sorted global relexp (#1773)

--- src/PKSim.Presentation/DTO/Mappers/ExpressionParametersToSimulationExpressionsDTOMapper.cs
+++ src/PKSim.Presentation/DTO/Mappers/ExpressionParametersToSimulationExpressionsDTOMapper.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using OSPSuite.Core.Domain;
 using OSPSuite.Utility;
 using OSPSuite.Utility.Extensions;
+using PKSim.Core;
 using PKSim.Core.Model;
 using PKSim.Presentation.DTO.Individuals;
 using PKSim.Presentation.DTO.Simulations;
@@ -28,8 +29,13 @@ namespace PKSim.Presentation.DTO.Mappers
          var allParameters = expressionParameters.ToList();
          var allGlobalParameters = allParameters.AllGlobalMoleculeParameters();
 
-         return new SimulationExpressionsDTO(allGlobalParameters, allParameters.Except(allGlobalParameters).MapAllUsing(_expressionContainerMapper)
-         );
+         var allExpressionParametersDTOs = allParameters.Except(allGlobalParameters).MapAllUsing(_expressionContainerMapper)
+            .OrderBy(x => x.Sequence)
+            //we do not want to disrupt the sequence that is based on physiological order except for global surrogate where the container is not defined
+            .ThenBy(x => string.IsNullOrEmpty(x.ContainerName) ? x.CompartmentName : x.Sequence.ToString())
+            .ToArray();
+
+         return new SimulationExpressionsDTO(allGlobalParameters, allExpressionParametersDTOs);
       }
    }
 }
