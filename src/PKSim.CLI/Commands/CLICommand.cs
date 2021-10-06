4e73a3fd83875939099fa8935d1c05672ddc3d42
Michael Sevestre <michael@design2code.ca>
2021-10-06 16:23:41 -0400

    Fixes #1888 nopop for csv (#1889)
    * Fixes #1888 nopop for csv
    
    * Fixes #1888 also add aging data

--- src/PKSim.CLI/Commands/CLICommand.cs
+++ src/PKSim.CLI/Commands/CLICommand.cs
@@ -15,9 +15,6 @@ namespace PKSim.CLI.Commands
       [Option('l', "log", Required = false, HelpText = "Optional. Full path of log files where log output will be written. A log file will not be created if this value is not provided.")]
       public IEnumerable<string> LogFilesFullPath { get; set; } = new string[] { };
 
-      [Option('a', "append", Required = false, HelpText = "Optional. true to append data to the file; false to overwrite the file (default). If the specified file does not exist, this parameter has no effect, and a new file is created. ")]
-      public bool AppendToLog { get; set; }
-
       [Option("logLevel", Required = false, HelpText = "Optional. Log verbosity (Debug, Information, Warning, Error). Default is Information.")]
       public LogLevel LogLevel { get; set; } = LogLevel.Information;
 
