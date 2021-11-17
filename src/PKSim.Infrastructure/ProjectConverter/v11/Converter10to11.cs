96e0fa7e03f1e14a40e318327537377e018591a8
Michael Sevestre <michael@design2code.ca>
2021-11-17 17:15:55 -0500

    WIP 1920 project conversion. Add test

---
+++ src/PKSim.Infrastructure/ProjectConverter/v11/Converter10to11.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+using PKSim.Core;
+
+namespace PKSim.Infrastructure.ProjectConverter.v11
+{
+   public class Converter10to11 : IObjectConverter
+   {
+      public bool IsSatisfiedBy(int version) => version == ProjectVersions.V10;
+
+      public (int convertedToVersion, bool conversionHappened) Convert(object objectToConvert, int originalVersion)
+      {
+         return (ProjectVersions.V11, false);
+      }
+
+      public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element, int originalVersion)
+      {
+         return (ProjectVersions.V11, false);
+      }
+   }
+}
---
+++ tests/PKSim.Tests/ProjectConverter/v11/Converter10to11Specs.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FakeItEasy;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using OSPSuite.Utility.Extensions;
+using PKSim.Core;
+using PKSim.Core.Model;
+using PKSim.Infrastructure.ProjectConverter.v10;
+using PKSim.Infrastructure.ProjectConverter.v11;
+using PKSim.IntegrationTests;
+
+namespace PKSim.ProjectConverter.v11
+{
+   public class When_converting_the_simple_project_730_project_to_11 : ContextWithLoadedProject<Converter10to11>
+   {
+      private List<PopulationSimulation> _allSimulations;
+      private List<Population> _allPopulations;
+      private List<Individual> _allIndividuals;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         LoadProject("SimplePop_73");
+         _allSimulations = All<PopulationSimulation>().ToList();
+         _allPopulations = All<Population>().ToList();
+         _allIndividuals = All<Individual>().ToList();
+         _allSimulations.Each(Load);
+         _allPopulations.Each(Load);
+         _allIndividuals.Each(Load);
+      }
+
+      [Observation]
+      public void should_have_created_an_expression_profile_for_the_individual()
+      {
+         var ind = _allIndividuals.FindByName("Human");
+         var cyp3A4 = ind.MoleculeByName<IndividualEnzyme>("CYP3A4");
+         var expressionProfile = FindByName<ExpressionProfile>(CoreConstants.ContainerName.ExpressionProfileName(cyp3A4.Name, ind.Species, ind.Name));
+         expressionProfile.ShouldNotBeNull();
+      }
+   }
+
+}
