4042acb3125457897b443c6fea22dc562c68b892
Michael Sevestre <michael@design2code.ca>
2021-12-01 14:40:34 -0500

    Fixes ivnestigate differences in batch runs

--- tests/PKSim.Tests/IntegrationTests/IndividualMapperSpecs.cs
+++ tests/PKSim.Tests/IntegrationTests/IndividualMapperSpecs.cs
@@ -92,4 +92,50 @@ namespace PKSim.IntegrationTests
          _individual.AllExpressionProfiles().ShouldContain(_expressionProfileEnzyme, _expressionProfileTransporter);
       }
    }
+
+   public class When_loading_a_snapshot_file_containing_expression_created_in_v9 : ContextWithLoadedSnapshot
+   {
+      private Individual _individual;
+      private ExpressionProfile _expressionProfileCYP3A4;
+      private ExpressionProfile _expressionProfileCYP2C8;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         LoadSnapshot("ind_expression_v9");
+         _individual = FindByName<Individual>("Ind");
+         _expressionProfileCYP3A4 = FindByName<ExpressionProfile>("CYP3A4|Human|Ind");
+         _expressionProfileCYP2C8 = FindByName<ExpressionProfile>("CYP2C8|Human|Ind");
+      }
+
+      [Observation]
+      public void should_have_created_an_expression_profile_based_on_the_value_defined_in_the_snapshot_file_for_the_enzyme_cyp3A4()
+      {
+         _expressionProfileCYP3A4.ShouldNotBeNull();
+         var (enzyme, individual) = _expressionProfileCYP3A4;
+         enzyme.Ontogeny.Name.ShouldBeEqualTo("CYP3A4");
+         enzyme.ReferenceConcentration.ValueInDisplayUnit.ShouldBeEqualTo(4.32);
+         var expressionInBone = individual.Organism.EntityAt<MoleculeExpressionContainer>("Brain", "Intracellular", enzyme.Name);
+         expressionInBone.RelativeExpressionParameter.Value.ShouldBeEqualTo(0.004168, 1e-2);
+      }
+
+      [Observation]
+      public void should_have_created_an_expression_profile_based_on_the_value_defined_in_the_snapshot_file_for_the_enzyme_cyp2C8()
+      {
+         _expressionProfileCYP2C8.ShouldNotBeNull();
+         var (enzyme, individual) = _expressionProfileCYP2C8;
+         enzyme.Ontogeny.Name.ShouldBeEqualTo("CYP2C8");
+         enzyme.ReferenceConcentration.ValueInDisplayUnit.ShouldBeEqualTo(2.56);
+         enzyme.HalfLifeIntestine.ValueInDisplayUnit.ShouldBeEqualTo(23);
+         var expressionInBone = individual.Organism.EntityAt<MoleculeExpressionContainer>("Kidney", "Intracellular", enzyme.Name);
+         expressionInBone.RelativeExpressionParameter.Value.ShouldBeEqualTo(0.0012661, 1e-2);
+      }
+
+      [Observation]
+      public void should_have_added_the_newly_created_expression_profile_as_reference_in_the_individual()
+      {
+         _individual.AllExpressionProfiles().ShouldContain(_expressionProfileCYP3A4, _expressionProfileCYP2C8);
+      }
+   }
+
 }
