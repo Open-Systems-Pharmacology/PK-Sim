780cfc0f1dd1e98cab65e088fea914d2b2839f03
georgeDaskalakis <geodas13@gmail.com>
2022-01-31 12:06:01 +0100

    updating pk-sim

--- src/PKSim.Infrastructure/Services/ImportObservedDataTask.cs
+++ src/PKSim.Infrastructure/Services/ImportObservedDataTask.cs
@@ -310,7 +310,7 @@ namespace PKSim.Infrastructure.Services
 
       public IReadOnlyList<string> ReadOnlyMetaDataCategories => new List<string> { };
 
-      public bool MolWeightEditable => false;
+      public bool MolWeightAlwaysEditable => false;
 
       public bool MolWeightVisible => true;
 
