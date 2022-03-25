61c5946911b05a84fa8220d23e20b55c8b688b94
Michael Sevestre <michael@design2code.ca>
2022-03-25 16:10:58 -0400

    2138 plot title and description not copied (#2139)
    * Fixes #2138 plot title and description not copied
    
    * Fixes #2138 plot title and description

--- src/PKSim.Core/Chart/SummaryChart.cs
+++ src/PKSim.Core/Chart/SummaryChart.cs
@@ -1,9 +1,6 @@
 using System.Collections.Generic;
 using OSPSuite.Core.Chart;
-using OSPSuite.Core.Domain;
-using OSPSuite.Core.Domain.Services;
 using OSPSuite.Utility.Collections;
-using OSPSuite.Utility.Visitor;
 using PKSim.Core.Model;
 
 namespace PKSim.Core.Chart
@@ -12,7 +9,6 @@ namespace PKSim.Core.Chart
    {
       private readonly ICache<string, IndividualSimulation> _allSimulations;
       public bool IsLoaded { get; set; }
-      public string Icon { get; set; }
 
       public IReadOnlyCollection<IndividualSimulation> AllSimulations => _allSimulations;
 
@@ -49,18 +45,6 @@ namespace PKSim.Core.Chart
       public void RemoveAllSimulations()
       {
          _allSimulations.Clear();
-      }
-
-      public override void AcceptVisitor(IVisitor visitor)
-      {
-         visitor.Visit(this);
-      }
-
-      public void UpdatePropertiesFrom(IUpdatable source, ICloneManager cloneManager)
-      {
-         var originalChart = source as SummaryChart;
-         if (originalChart == null) return;
-         UpdateFrom(originalChart);
       }
    }
 }
