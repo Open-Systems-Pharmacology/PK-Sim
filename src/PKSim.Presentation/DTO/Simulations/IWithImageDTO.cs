94d4ba2166eff1fc6e332c8c7ef2b8b24fc3ad70
Michael Sevestre <michael@design2code.ca>
2022-03-28 11:36:41 -0400

    Fixes #2143 missing icons

---
+++ src/PKSim.Presentation/DTO/Simulations/IWithImageDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+using DevExpress.XtraEditors.DXErrorProvider;
+using OSPSuite.Assets;
+
+namespace PKSim.Presentation.DTO.Simulations
+{
+   public interface IWithImageDTO : IDXDataErrorInfo
+   {
+      Image Image { get; }
+   }
+}
