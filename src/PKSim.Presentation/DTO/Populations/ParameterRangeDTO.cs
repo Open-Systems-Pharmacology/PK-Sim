a4bdff5448ab260fc1ea0cfed91ba853a89400bc
Michael Sevestre <michael@design2code.ca>
2022-04-08 11:34:56 -0400

    Fixes #2183 wrong error range

--- src/PKSim.Presentation/DTO/Populations/ParameterRangeDTO.cs
+++ src/PKSim.Presentation/DTO/Populations/ParameterRangeDTO.cs
@@ -9,7 +9,7 @@ namespace PKSim.Presentation.DTO.Populations
 {
    public class ParameterRangeDTO : DxValidatableDTO<ParameterRange>
    {
-      public ParameterRange ParameterRange { get; private set; }
+      public ParameterRange ParameterRange { get; }
 
       public ParameterRangeDTO(ParameterRange parameterRange)
          : base(parameterRange)
@@ -17,31 +17,25 @@ namespace PKSim.Presentation.DTO.Populations
          ParameterRange = parameterRange;
       }
 
-      public string ParameterName
-      {
-         get { return ParameterRange.ParameterName; }
-      }
+      public string ParameterName => ParameterRange.ParameterName;
 
-      public string ParameterDisplayName
-      {
-         get { return ParameterRange.ParameterDisplayName; }
-      }
+      public string ParameterDisplayName => ParameterRange.ParameterDisplayName;
 
       public double? MinValueInDisplayUnit
       {
-         get { return ParameterRange.MinValueInDisplayUnit; }
-         set { ParameterRange.MinValueInDisplayUnit = value; }
+         get => ParameterRange.MinValueInDisplayUnit;
+         set => ParameterRange.MinValueInDisplayUnit = value;
       }
 
       public double? MaxValueInDisplayUnit
       {
-         get { return ParameterRange.MaxValueInDisplayUnit; }
-         set { ParameterRange.MaxValueInDisplayUnit = value; }
+         get => ParameterRange.MaxValueInDisplayUnit;
+         set => ParameterRange.MaxValueInDisplayUnit = value;
       }
 
       public Unit Unit
       {
-         get { return ParameterRange.Unit; }
+         get => ParameterRange.Unit;
          set
          {
             var minDisplayValue = MinValueInDisplayUnit;
@@ -52,10 +46,7 @@ namespace PKSim.Presentation.DTO.Populations
          }
       }
 
-      public bool IsDiscrete
-      {
-         get { return ParameterRange.IsAnImplementationOf<DiscreteParameterRange>(); }
-      }
+      public bool IsDiscrete => ParameterRange.IsAnImplementationOf<DiscreteParameterRange>();
 
       public IEnumerable<double> ListOfValues
       {
