081895cf881d27e9c87f3a0215195e9897916bdb
Michael Sevestre <michael@design2code.ca>
2022-04-14 09:59:31 -0400

    Fixes #2202 rename expression profile

--- src/PKSim.UI/Views/ExpressionProfiles/RenameExpressionProfileView.cs
+++ src/PKSim.UI/Views/ExpressionProfiles/RenameExpressionProfileView.cs
@@ -1,4 +1,4 @@
-﻿using OSPSuite.Assets;
+﻿using System.Windows.Forms;
 using OSPSuite.DataBinding;
 using OSPSuite.DataBinding.DevExpress;
 using OSPSuite.Presentation.Extensions;
@@ -15,25 +15,28 @@ namespace PKSim.UI.Views.ExpressionProfiles
    public partial class RenameExpressionProfileView : BaseModalView, IRenameExpressionProfileView
    {
       private readonly ScreenBinder<ExpressionProfileDTO> _screenBinder = new ScreenBinder<ExpressionProfileDTO>();
+      private IRenameExpressionProfilePresenter _presenter;
 
       //only for design time
       public RenameExpressionProfileView() : this(null)
       {
       }
 
-      public RenameExpressionProfileView(IShell shell):base(shell)
+      public RenameExpressionProfileView(IShell shell) : base(shell)
       {
          InitializeComponent();
       }
 
       public void AttachPresenter(IRenameExpressionProfilePresenter presenter)
       {
+         _presenter = presenter;
       }
 
       public void BindTo(ExpressionProfileDTO expressionProfileDTO)
       {
          ApplicationIcon = expressionProfileDTO.Icon;
          cbMoleculeName.FillWith(expressionProfileDTO.AllMolecules);
+         cbCategory.FillWith(expressionProfileDTO.AllCategories);
          layoutItemMoleculeName.Text = expressionProfileDTO.MoleculeType.FormatForLabel();
          _screenBinder.BindToSource(expressionProfileDTO);
       }
@@ -45,7 +48,7 @@ namespace PKSim.UI.Views.ExpressionProfiles
             .To(cbMoleculeName);
 
          _screenBinder.Bind(x => x.Category)
-            .To(tbCategory);
+            .To(cbCategory);
 
          RegisterValidationFor(_screenBinder);
       }
@@ -56,11 +59,17 @@ namespace PKSim.UI.Views.ExpressionProfiles
       {
          base.InitializeResources();
          layoutItemCategory.Text = PKSimConstants.UI.ExpressionProfileCategory.FormatForLabel();
+
+         //Do not close on OK
+         ButtonOk.DialogResult = DialogResult.None;
       }
 
       protected override void SetActiveControl()
       {
          ActiveControl = cbMoleculeName;
       }
+
+      protected override void OkClicked() => _presenter.Save();
+
    }
 }
