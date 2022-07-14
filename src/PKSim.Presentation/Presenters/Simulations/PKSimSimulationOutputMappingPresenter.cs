3a29a723ad82617e5c07200dea18d47543d778ec
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-07-14 15:01:47 +0200

    correcting the presenter

--- src/PKSim.Presentation/Presenters/Simulations/PKSimSimulationOutputMappingPresenter.cs
+++ src/PKSim.Presentation/Presenters/Simulations/PKSimSimulationOutputMappingPresenter.cs
@@ -1,24 +1,15 @@
-using System.Collections.Generic;
-using OSPSuite.Assets;
-using OSPSuite.Core.Commands.Core;
 using OSPSuite.Core.Domain.Repositories;
 using OSPSuite.Core.Domain.Services;
 using OSPSuite.Core.Domain.Services.ParameterIdentifications;
 using OSPSuite.Presentation.Mappers;
-using OSPSuite.Presentation.Mappers.ParameterIdentifications;
 using PKSim.Core.Model;
 using OSPSuite.Presentation.Presenters;
 using OSPSuite.Presentation.Views;
 
 namespace PKSim.Presentation.Presenters.Simulations
 {
-   public interface IPKSimSimulationOutputMappingPresenter :  IEditIndividualSimulationItemPresenter, IEditPopulationSimulationItemPresenter
+   public interface IPKSimSimulationOutputMappingPresenter :  IEditIndividualSimulationItemPresenter//, IEditPopulationSimulationItemPresenter
    {
-      /// <summary>
-      ///    Sets the subject of the mapped Outputs to the simulation
-      /// </summary>
-      /// <param name="simulation">The simulation containing the mapped outputs being displayed</param>
-      void Edit(Simulation simulation);
    }
 
    public class PKSimSimulationOutputMappingPresenter : SimulationOutputMappingPresenter, IPKSimSimulationOutputMappingPresenter
@@ -29,35 +20,7 @@ namespace PKSim.Presentation.Presenters.Simulations
 
       public void EditSimulation(IndividualSimulation simulation)
       {
-         Edit(simulation);
-      }
-
-      public void EditSimulation(PopulationSimulation simulation)
-      {
-         Edit(simulation);
-      }
-
-      public void Edit(Simulation simulation)
-      {
          SetSimulation(simulation);
       }
-
-      public void InitializeWith(ICommandCollector initializer)
-      {
-         //
-      }
-
-      public void AddCommand(ICommand command)
-      {
-         //
-      }
-
-      public IEnumerable<ICommand> All()
-      {
-         return new List<ICommand>();
-      }
-
-      public ICommandCollector CommandCollector { get; }
-      public ApplicationIcon Icon { get; }
    }
 }
