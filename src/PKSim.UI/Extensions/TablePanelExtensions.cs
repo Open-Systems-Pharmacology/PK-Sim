dc802d49de2b8bc5ecb1a7de3858a0d8de6e7fc3
Michael Sevestre <michael@design2code.ca>
2022-07-29 18:55:46 -0400

    Fixes #2249 scale button

---
+++ src/PKSim.UI/Extensions/TablePanelExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using DevExpress.Utils.Layout;
+
+namespace PKSim.UI.Extensions
+{
+   public static class TablePanelExtensions
+   {
+      
+   // public static void InitializeDisabledColors(this TablePanel tablePanel, UserLookAndFeel lookAndFeel)
+   // {
+   //    var currentSkin = CommonSkins.GetSkin(lookAndFeel);
+   //    var clrText = currentSkin.Colors[CommonColors.WindowText];
+   //       tablePanel.Appearance. .ControlDisabled.ForeColor = clrText;
+   //    tablePanel.Appearance.DisabledLayoutItem.ForeColor = clrText;
+   //    tablePanel.Appearance.DisabledLayoutGroupCaption.ForeColor = clrText;
+   // }
+}
+}
