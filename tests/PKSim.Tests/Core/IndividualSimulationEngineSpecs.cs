541acfe2d03d031c4e329dd126682d09691d06fc
Michael Sevestre <michael@design2code.ca>
2022-12-17 13:29:16 -0500

    Fixes buil failing (#2489)

--- tests/PKSim.Tests/Core/IndividualSimulationEngineSpecs.cs
+++ tests/PKSim.Tests/Core/IndividualSimulationEngineSpecs.cs
@@ -57,7 +57,7 @@ namespace PKSim.Core
          _simulation.Name = "Hello";
          _simulation.DataRepository = new DataRepository();
          _simulation.AucIV["TOTO"] = 55;
-         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults(true, Enumerable.Empty<SolverWarning>(), new DataRepository()));
+         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults(Enumerable.Empty<SolverWarning>(), new DataRepository()));
       }
 
       protected override Task Because()
@@ -105,7 +105,7 @@ namespace PKSim.Core
       {
          await base.Context();
          _simulation = A.Fake<IndividualSimulation>();
-         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults(true, Enumerable.Empty<SolverWarning>(), new DataRepository()));
+         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults( Enumerable.Empty<SolverWarning>(), new DataRepository()));
          await sut.RunAsync(_simulation, _simulationRunOption);
       }
 
@@ -131,7 +131,7 @@ namespace PKSim.Core
          await base.Context();
          _simulation = A.Fake<IndividualSimulation>();
          _simulationRunOption.RaiseEvents = false;
-         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults(true, Enumerable.Empty<SolverWarning>(), new DataRepository()));
+         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults( Enumerable.Empty<SolverWarning>(), new DataRepository()));
       }
 
       protected override Task Because()
@@ -156,7 +156,7 @@ namespace PKSim.Core
          await base.Context();
          _dataRepository = A.Fake<DataRepository>();
          _simulation = A.Fake<IndividualSimulation>();
-         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults(false, Enumerable.Empty<SolverWarning>(), _dataRepository));
+         A.CallTo(_simModelManager).WithReturnType<SimulationRunResults>().Returns(new SimulationRunResults(Enumerable.Empty<SolverWarning>(), _dataRepository));
          await sut.RunAsync(_simulation, _simulationRunOption);
       }
 
