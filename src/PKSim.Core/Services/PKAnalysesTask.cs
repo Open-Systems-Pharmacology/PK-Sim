3311fea3bebbdbf76acff547becf571e3e092e29
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-01-23 13:57:00 -0500

    PR feedback

--- src/PKSim.Core/Services/PKAnalysesTask.cs
+++ src/PKSim.Core/Services/PKAnalysesTask.cs
@@ -904,7 +904,7 @@ namespace PKSim.Core.Services
 
       private static bool canBeUsedToCalculatePK(QuantityPKParameter pkParameter)
       {
-         return pkParameter.ValuesAsArray.Any(x => x.IsValid());
+         return pkParameter.ValuesAsArray.All(x => x.IsValid());
       }
 
       private string correctNameFromMetric(string originalText, bool multipleValues, bool isLowerValue, string captionPrefix)
