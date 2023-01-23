8b3456f67ab43e7af31d7919e028f67b6ca20382
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-01-23 12:24:34 -0500

    Fixes #2518 11.1 Cannot show PK-Analysis in a population simulations imported from pkml

--- src/PKSim.Core/Services/PKAnalysesTask.cs
+++ src/PKSim.Core/Services/PKAnalysesTask.cs
@@ -878,8 +878,18 @@ namespace PKSim.Core.Services
       {
          var pkParametersList = pkParameters.ToList();
          var matrix = new FloatMatrix();
-         var names = pkParametersList.Select(x => x.Name).Distinct().ToList();
-         pkParametersList.Each(pkParameter => matrix.AddValuesAndSort(pkParameter.ValuesAsArray));
+         var names = new List<string>();
+
+         pkParametersList.Each(pkParameter =>
+         {
+            if (!pkParameter.ValuesAsArray.All(x => x.IsValid())) 
+               return;
+
+            matrix.AddValuesAndSort(pkParameter.ValuesAsArray);
+            names.Add(pkParameter.Name);
+         });
+
+         var distinctNames = names.Distinct().ToList();
 
          var results = new List<PopulationPKAnalysis>();
          selectedStatistics.Each(statisticalAnalysis =>
@@ -888,7 +898,7 @@ namespace PKSim.Core.Services
             aggregated.Each((agg, index) =>
             {
                var name = correctNameFromMetric(_representationInfoRepository.DisplayNameFor(statisticalAnalysis), aggregated.Count > 1, index == 0, captionPrefix);
-               var pkAnalysis = buildPopulationPKAnalysis(buildCurveData(pkParametersList[index], name), agg, names, simulation);
+               var pkAnalysis = buildPopulationPKAnalysis(buildCurveData(pkParametersList[index], name), agg, distinctNames, simulation);
                results.Add(pkAnalysis);
             });
          });
