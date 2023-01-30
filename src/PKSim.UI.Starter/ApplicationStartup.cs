862323d0c570686f2e65b640c76c06e8b9d51362
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2023-01-30 23:20:32 +0100

    initial implementation

--- src/PKSim.UI.Starter/ApplicationStartup.cs
+++ src/PKSim.UI.Starter/ApplicationStartup.cs
@@ -8,22 +8,43 @@ using OSPSuite.Infrastructure.Import.Services;
 using OSPSuite.Presentation;
 using OSPSuite.Presentation.Core;
 using OSPSuite.Presentation.DTO;
+using OSPSuite.Presentation.Presenters;
+using OSPSuite.Presentation.Presenters.ContextMenus;
 using OSPSuite.Presentation.Services;
 using OSPSuite.Presentation.Views;
+using OSPSuite.UI.Binders;
 using OSPSuite.UI.Mappers;
 using OSPSuite.UI.Services;
+using OSPSuite.UI.Views;
 using OSPSuite.Utility.Container;
 using OSPSuite.Utility.Exceptions;
+using OSPSuite.Utility.Format;
 using PKSim.CLI.Core.MinimalImplementations;
 using PKSim.Core;
+using PKSim.Core.Model;
 using PKSim.Core.Services;
 using PKSim.Infrastructure;
 using PKSim.Presentation;
+using PKSim.Presentation.DTO.Core;
+using PKSim.Presentation.DTO.Individuals;
 using PKSim.Presentation.DTO.Mappers;
+using PKSim.Presentation.DTO.Parameters;
+using PKSim.Presentation.Mappers;
+using PKSim.Presentation.Nodes;
 using PKSim.Presentation.Presenters.ExpressionProfiles;
+using PKSim.Presentation.Presenters.Individuals;
+using PKSim.Presentation.Presenters.Individuals.Mappers;
+using PKSim.Presentation.Presenters.Parameters;
+using PKSim.Presentation.Presenters.Parameters.Mappers;
 using PKSim.Presentation.Services;
 using PKSim.Presentation.Views.ExpressionProfiles;
+using PKSim.Presentation.Views.Individuals;
+using PKSim.Presentation.Views.Parameters;
+using PKSim.UI.Views;
 using PKSim.UI.Views.ExpressionProfiles;
+using PKSim.UI.Views.Individuals;
+using PKSim.UI.Views.Parameters;
+using RenameObjectDTOFactory = OSPSuite.Presentation.DTO.RenameObjectDTOFactory;
 
 namespace PKSim.UI.Starter
 {
@@ -54,17 +75,20 @@ namespace PKSim.UI.Starter
          container.Register<IApplicationController, ApplicationController>(LifeStyle.Singleton);
          container.Register<ICoreWorkspace, OSPSuite.Core.IWorkspace, IWorkspace, Workspace>(LifeStyle.Singleton);
 
-         // registerImplementationsFromCore(container);
          registerMinimalImplementations(container);
 
 
          using (container.OptimizeDependencyResolution())
          {
+            var PKSimShell = new Shell(container);
             container.RegisterImplementationOf(shell);
+            container.RegisterImplementationOf(PKSimShell);
             container.AddRegister(x => x.FromType<CoreRegister>());
             container.AddRegister(x => x.FromType<InfrastructureRegister>());
             container.Register<ICreateExpressionProfilePresenter, CreateExpressionProfilePresenter>();
             container.Register<ICreateExpressionProfileView, CreateExpressionProfileView>();
+            container.Register<ICreateIndividualPresenter, CreateIndividualPresenter>();
+            container.Register<ICreateIndividualView, CreateIndividualView>();
             container.Register<IExpressionProfileToExpressionProfileDTOMapper, ExpressionProfileToExpressionProfileDTOMapper>();
             container.Register<IMoleculePropertiesMapper, MoleculePropertiesMapper>();
             container.Register<ICoreUserSettings, IPresentationUserSettings, UIStarterUserSettings>(LifeStyle.Singleton);
@@ -72,6 +96,72 @@ namespace PKSim.UI.Starter
             container.Register<IEntityTask, EntityTask>();
             container.Register<IRenameObjectDTOFactory, RenameObjectDTOFactory>();
             container.Register<IHistoryManager, HistoryManager<IExecutionContext>>();
+            container.Register<ISubPresenterItemManager<IIndividualItemPresenter>, SubPresenterItemManager<IIndividualItemPresenter>>();
+            container.Register<IBuildingBlockPropertiesMapper, BuildingBlockPropertiesMapper>();
+            container.Register<IObjectBaseDTOFactory, ObjectBaseDTOFactory>();
+            container.Register<IIndividualSettingsPresenter, IndividualSettingsPresenter>();
+            container.Register<IIndividualSettingsView, IndividualSettingsView>();
+            container.Register<IToolTipCreator, ToolTipCreator>();
+            container.Register<IToolTipPartCreator, ToolTipPartCreator>();
+            container.Register<IReportPartToToolTipPartsMapper, ReportPartToToolTipPartsMapper>();
+            container.Register<IIndividualDefaultValueUpdater, IndividualDefaultValuesUpdater>();
+            container.Register<IIndividualSettingsDTOToOriginDataMapper, IndividualSettingsDTOToOriginDataMapper>();
+            container.Register<IParameterToParameterDTOMapper, ParameterToParameterDTOMapper>();
+            container.Register<ISubPopulationToSubPopulationDTOMapper, SubPopulationToSubPopulationDTOMapper>();
+            container
+               .Register<IParameterValueVersionToCategoryParameterValueVersionDTOMapper,
+                  ParameterValueVersionToCategoryParameterValueVersionDTOMapper>();
+            container.Register<ICalculationMethodToCategoryCalculationMethodDTOMapper, CalculationMethodToCategoryCalculationMethodDTOMapper>();
+            container.Register<IIndividualToIIndividualSettingsDTOMapper, IndividualToIIndividualSettingsDTOMapper>();
+            container.Register<IOriginDataParameterToParameterDTOMapper, OriginDataParameterToParameterDTOMapper>();
+            container.Register<IIndividualSettingsDTOToIndividualMapper, IndividualSettingsDTOToIndividualMapper>();
+            container.Register<IEditValueOriginPresenter, EditValueOriginPresenter>();
+            container.Register<IEditValueOriginView, EditValueOriginView>();
+            container.Register<ValueOriginBinder<ValueOriginDTO>, ValueOriginBinder<ValueOriginDTO>>();
+            container.Register<IValueOriginPresenter, ValueOriginPresenter>();
+            container.Register<IValueOriginView, ValueOriginView>();
+            container.Register<OSPSuite.UI.Services.IToolTipCreator, ToolTipCreator>();
+            container.Register<IIndividualParametersPresenter, IndividualParametersPresenter>();
+            container.Register<IIndividualParametersView, IndividualParametersView>();
+            container.Register<IParameterGroupsPresenter, ParameterGroupsPresenter>();
+            container.Register<IParameterGroupsView, ParameterGroupsView>();
+            container.Register<IParameterGroupNodeCreator, ParameterGroupNodeCreator>();
+            container.Register<IParameterGroupToTreeNodeMapper, ParameterGroupToTreeNodeMapper>();
+            container.Register<ITreeNodeFactory, TreeNodeFactory>();
+            container.Register<IDynamicGroupExpander, DynamicGroupExpander>();
+            container.Register<IParameterContainerToTreeNodeMapper, ParameterContainerToTreeNodeMapper>();
+            container.Register<INodeToCustomizableParametersPresenterMapper, NodeToCustomizableParametersPresenterMapper>();
+            container.Register<IContainerToCustomableParametersPresenterMapper, ContainerToCustomableParametersPresenterMapper>();
+            container.Register<IParameterGroupToCustomizableParametersPresenter, ParameterGroupToCustomizableParametersPresenter>();
+            container.RegisterFactory<IMultiParameterEditPresenterFactory>();
+            container.Register<INoItemInSelectionPresenter, NoItemInSelectionPresenter>();
+            container.Register<INoItemInSelectionView, NoItemInSelectionView>();
+            container.Register<IUserSettings, UserSettings>();
+            container.Register<INumericFormatterOptions, NumericFormatterOptions>();
+            container.Register<ISkinManager, SkinManager>();
+            container.Register<DirectoryMapSettings, DirectoryMapSettings>();
+            container.Register<IPresentationSettingsTask, PresentationSettingsTask>();
+            container.Register<IWorkspace, Infrastructure.Workspace>();
+            container.Register<IWithWorkspaceLayout, Infrastructure.Workspace>();
+            container.Register<IWorkspaceLayout, WorkspaceLayout>();
+            container.Register<ITreeNodeContextMenuFactory, TreeNodeContextMenuFactory>();
+            container.Register<IIndividualMoleculesPresenter, IndividualMoleculesPresenter>();
+            container.Register<IMoleculesView, MoleculesView>();
+            container.Register<IEditMoleculeTask<Individual>, EditMoleculeTask<Individual>>();
+            container
+               .Register<IRootNodeToIndividualExpressionsPresenterMapper<Individual>, RootNodeToIndividualExpressionsPresenterMapper<Individual>>();
+            container.Register<IFavoriteParametersPresenter, FavoriteParametersPresenter>();
+            container.Register<IFavoriteParametersView, FavoriteParametersView>();
+            container.Register<IMultiParameterEditPresenter, MultiParameterEditPresenter>();
+            container.Register<IMultiParameterEditView, MultiParameterEditView>();
+            container.Register<ValueOriginBinder<ParameterDTO>, ValueOriginBinder<ParameterDTO>>();
+            container.Register<PathElementsBinder<ParameterDTO>, PathElementsBinder<ParameterDTO>>();
+            container.Register<IScaleParametersPresenter, ScaleParametersPresenter>();
+            container.Register<IScaleParametersView, ScaleParametersView>();
+            container.Register<IEditParameterPresenterTask, EditParameterPresenterTask>();
+            container.Register<IParameterContextMenuFactory, ParameterContextMenuFactory>();
+            container.Register<IIndividualToIndividualBuildingBlockMapper, IndividualToIndividualBuildingBlockMapper>();
+
             InfrastructureRegister.LoadSerializers(container);
          }
 
---
+++ src/PKSim.UI.Starter/IndividualCreator.cs
@@ -0,0 +1,27 @@
+﻿using OSPSuite.Core.Commands.Core;
+using OSPSuite.Presentation.Views;
+using PKSim.Core.Model;
+using PKSim.Presentation;
+using PKSim.Presentation.Mappers;
+using PKSim.Presentation.Presenters.Individuals;
+
+namespace PKSim.UI.Starter
+{
+   public static class IndividualCreator
+   {
+      public static object CreateIndividual(IShell shell)
+      {
+         var container = ApplicationStartup.Initialize(shell);
+
+         using (var presenter = container.Resolve<ICreateIndividualPresenter>())
+         {
+            presenter.Initialize();
+            var workspace = container.Resolve<IWorkspace>();
+            workspace.Project = new PKSimProject();
+            var mapper = container.Resolve<IIndividualToIndividualBuildingBlockMapper>();
+
+            return presenter.Create().IsEmpty() ? null : mapper.MapFrom(presenter.Individual);
+         }
+      }
+   }
+}
