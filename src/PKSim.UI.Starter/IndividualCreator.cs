b1fef67ad2b8ed66626347cd2cd694df69a71c16
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-03-21 07:12:12 -0400

    Fixes #2570 Move IndividualToIndividualBuildingBlockMapper from presentation to core (#2572)

--- src/PKSim.UI.Starter/IndividualCreator.cs
+++ src/PKSim.UI.Starter/IndividualCreator.cs
@@ -1,7 +1,7 @@
 ﻿using OSPSuite.Core.Commands.Core;
+using PKSim.Core.Mappers;
 using PKSim.Core.Model;
 using PKSim.Presentation;
-using PKSim.Presentation.Mappers;
 using PKSim.Presentation.Presenters.Individuals;
 
 namespace PKSim.UI.Starter
@@ -17,10 +17,10 @@ namespace PKSim.UI.Starter
             presenter.Initialize();
             var workspace = container.Resolve<IWorkspace>();
             workspace.Project = new PKSimProject();
-            
-            if (presenter.Create().IsEmpty()) 
+
+            if (presenter.Create().IsEmpty())
                return null;
-            
+
             var mapper = container.Resolve<IIndividualToIndividualBuildingBlockMapper>();
             return mapper.MapFrom(presenter.Individual);
          }
