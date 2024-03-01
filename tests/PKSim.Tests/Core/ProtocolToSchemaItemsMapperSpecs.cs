e009e4f07e0e74d287157df486d7294d5e6978fb
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-03-01 10:08:12 -0500

    Fixes #2632 error value cannot be null

---
+++ tests/PKSim.Tests/Core/ProtocolToSchemaItemsMapperSpecs.cs
@@ -0,0 +1,30 @@
+﻿using FakeItEasy;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using PKSim.Core.Mappers;
+using PKSim.Core.Services;
+
+namespace PKSim.Core
+{
+   public abstract class concern_for_ProtocolToSchemaItemsMapper : ContextSpecification<IProtocolToSchemaItemsMapper>
+   {
+      private ISimpleProtocolToSchemaMapper _simpleMapper;
+      private ICloner _cloneManager;
+
+      protected override void Context()
+      {
+         _simpleMapper = A.Fake<ISimpleProtocolToSchemaMapper>();
+         _cloneManager = A.Fake<ICloner>();
+         sut = new ProtocolToSchemaItemsMapper(_simpleMapper, _cloneManager);
+      }
+   }
+
+   public class When_mapping_a_null_protocol_to_schema_item : concern_for_ProtocolToSchemaItemsMapper
+   {
+      [Observation]
+      public void should_return_an_empty_list_of_schema_items()
+      {
+         sut.MapFrom(null).ShouldBeEmpty();
+      }
+   }
+}
