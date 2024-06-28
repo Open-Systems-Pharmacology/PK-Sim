09529977b0d7ea0fc30d71881e5d1a852e18e35a
benjaperez1983 <benjamin.perez@gmail.com>
2024-06-28 14:22:35 +0200

    #2909 naming and fixes

--- src/PKSimRDependencyResolution/Program.cs
+++ src/PKSimRDependencyResolution/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("This console application includes all the dependencies needed for PK-SIM");
+         Console.WriteLine("This console application includes all the dependencies needed for PK-Sim");
       }
    }
 }
