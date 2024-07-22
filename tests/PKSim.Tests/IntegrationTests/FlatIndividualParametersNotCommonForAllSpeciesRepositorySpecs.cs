209f1fffb458d8ea1497d544422996932dce2fda
Yuri05 <Yuri05@github.com>
2024-07-22 13:42:34 +0200

    Fixes Open-Systems-Pharmacology/MoBi/#1401

--- tests/PKSim.Tests/IntegrationTests/FlatIndividualParametersNotCommonForAllSpeciesRepositorySpecs.cs
+++ tests/PKSim.Tests/IntegrationTests/FlatIndividualParametersNotCommonForAllSpeciesRepositorySpecs.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using PKSim.Core;
 using PKSim.Infrastructure.ORM.FlatObjects;
 using PKSim.Infrastructure.ORM.Repositories;
 
@@ -11,19 +13,30 @@ namespace PKSim.IntegrationTests
    {
    }
 
-   public class When_resolving_all_parameters_not_common_for_all_species_as_a_flat_table : concern_for_FlatIndividualParametersNotCommonForAllSpeciesRepository
+   public class
+      When_resolving_all_parameters_not_common_for_all_species_as_a_flat_table :
+      concern_for_FlatIndividualParametersNotCommonForAllSpeciesRepository
    {
       private IEnumerable<FlatIndividualParametersNotCommonForAllSpecies> _result;
+      private IEnumerable<FlatIndividualParametersNotCommonForAllSpecies> _organismParameters;
 
       protected override void Because()
       {
          _result = sut.All();
+         _organismParameters = _result.Where(p => p.ContainerName.Equals("Organism"));
       }
 
       [Observation]
       public void should_retrieve_some_object_from_the_underlying_database()
       {
          _result.Count().ShouldBeGreaterThan(0);
+      }
+
+      [Observation]
+      public void should_contain_mean_height()
+      {
+         _organismParameters.Count(p => p.ParameterName.Equals(CoreConstants.Parameters.MEAN_HEIGHT))
+            .ShouldBeEqualTo(1);
       }
    }
 }
