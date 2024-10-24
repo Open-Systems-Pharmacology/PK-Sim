1ea73de2b1f4b7280f76299d3c68828bca6f6eaa
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-10-24 14:16:38 -0400

    update code removed from core

--- src/PKSim.Core/Mappers/MoleculeToQueryExpressionSettingsMapper.cs
+++ src/PKSim.Core/Mappers/MoleculeToQueryExpressionSettingsMapper.cs
@@ -33,7 +33,7 @@ namespace PKSim.Core.Mappers
       public QueryExpressionSettings MapFrom(ExpressionProfileBuildingBlock expressionProfileBuildingBlock)
       {
          var expressionContainers = expressionProfileBuildingBlock.
-            ExpressionParameters.Where(x => x.Value.HasValue && x.HasExpressionName()).
+            ExpressionParameters.Where(x => x.Value.HasValue && HasExpressionName(x)).
             Select(expressionContainerInfoForExpressionParameter).ToList();
 
          return new QueryExpressionSettings(expressionContainers, string.Empty, expressionProfileBuildingBlock.MoleculeName);
@@ -50,6 +50,23 @@ namespace PKSim.Core.Mappers
          return new ExpressionContainerInfo(containerName,
             _representationInfoRepository.ContainerInfoFor(containerName).DisplayName,
             relExpParameter.Value);
+      }
+
+      public static readonly IReadOnlyList<string> AllGlobalRelExpParameters = new[]
+      {
+         Constants.Parameters.REL_EXP_BLOOD_CELLS,
+         Constants.Parameters.REL_EXP_PLASMA,
+         Constants.Parameters.REL_EXP_VASCULAR_ENDOTHELIUM,
+      };
+
+      public static bool HasGlobalExpressionName(ExpressionParameter withName)
+      {
+         return withName != null && AllGlobalRelExpParameters.Contains(withName.Name);
+      }
+
+      public static bool HasExpressionName(ExpressionParameter withName)
+      {
+         return withName != null && (HasGlobalExpressionName(withName) || withName.IsNamed(Constants.Parameters.REL_EXP));
       }
    }
 }
