101e63715a9d034d32557b9c2e19e2f1eaf51023
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-01-16 11:07:34 -0500

    a head start

--- src/PKSim.Presentation/UICommands/ReloadAllObservedDataCommand.cs
+++ src/PKSim.Presentation/UICommands/ReloadAllObservedDataCommand.cs
@@ -3,6 +3,9 @@ using OSPSuite.Presentation.UICommands;
 using PKSim.Core;
 using PKSim.Core.Services;
 using System.Linq;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Services.ParameterIdentifications;
+using OSPSuite.Utility.Extensions;
 
 namespace PKSim.Presentation.UICommands
 {
@@ -11,16 +14,18 @@ namespace PKSim.Presentation.UICommands
       private readonly IExecutionContext _executionContext;
       private readonly IImportObservedDataTask _importObservedDataTask;
       private readonly IObservedDataTask _observedDataTask;
+      private readonly IParameterIdentificationTask _parameterIdentificationTask;
 
       public ReloadAllObservedDataCommand(
          IExecutionContext executionContext,
          IImportObservedDataTask importObservedDataTask,
-         IObservedDataTask observedDataTask
-         )
+         IObservedDataTask observedDataTask,
+         IParameterIdentificationTask parameterIdentificationTask)
       {
          _executionContext = executionContext;
          _importObservedDataTask = importObservedDataTask;
          _observedDataTask = observedDataTask;
+         _parameterIdentificationTask = parameterIdentificationTask;
       }
 
       protected override void PerformExecute()
@@ -33,10 +38,22 @@ namespace PKSim.Presentation.UICommands
 
          //we should check this
          var observedDataFromSameFile =
-            project.AllObservedData.Where(r => !string.IsNullOrEmpty(r.ConfigurationId) && r.ConfigurationId == configurationId ); //actually the question here is: configID means they come from the same file right?
+            project.AllObservedData.Where(r => !string.IsNullOrEmpty(r.ConfigurationId) && r.ConfigurationId == configurationId ).ToList(); //actually the question here is: configID means they come from the same file right?
 
          var configuration = project.ImporterConfigurationBy(configurationId);
          _importObservedDataTask.AddAndReplaceObservedDataFromConfigurationToProject(configuration, observedDataFromSameFile);
+
+
+         observedDataFromSameFile.Each(observedData =>
+         {
+            _parameterIdentificationTask.ParameterIdentificationsUsingObservedData(observedData).Each(parameterIdentification =>
+            {
+               parameterIdentification.OutputMappingsUsingDataRepository(observedData).Each(outputMapping =>
+               {
+                  outputMapping.WeightedObservedData = new WeightedObservedData(observedData);
+               });
+            });
+         });
       }
    }
 }
