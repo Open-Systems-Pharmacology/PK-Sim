ad4d7129bc21e91551067f2ca3554a3e4bbcf6f2
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-02-11 11:38:14 -0500

    Fixes #3038 add "Ctrl+W" for "Close Tab" (#3078)

--- src/PKSim.UI/BootStrapping/PKSimApplication.cs
+++ src/PKSim.UI/BootStrapping/PKSimApplication.cs
@@ -9,16 +9,18 @@ using PKSim.Presentation;
 using PKSim.Presentation.Presenters.Main;
 using OSPSuite.Presentation.Core;
 using OSPSuite.Presentation.Presenters.Main;
+using Keys = System.Windows.Forms.Keys;
 
 namespace PKSim.UI.BootStrapping
 {
-   public class PKSimApplication : WindowsFormsApplicationBase
+   public class PKSimApplication : WindowsFormsApplicationBase, IMessageFilter
    {
       private readonly ApplicationStartup _applicationStartup;
       private readonly IEventPublisher _eventPublisher;
       private readonly IApplicationController _applicationController;
       private readonly StartOptions _startOptions;
       private ISplashViewPresenter _splashPresenter;
+      private IPKSimMainViewPresenter _mainViewPresenter;
 
       public PKSimApplication(IApplicationController applicationController, ApplicationStartup applicationStartup, IEventPublisher eventPublisher, StartOptions startOptions)
       {
@@ -43,9 +45,10 @@ namespace PKSim.UI.BootStrapping
 
       protected override void OnCreateMainForm()
       {
-         var mainViewPresenter = _applicationController.Start<IMainViewPresenter>().DowncastTo<IPKSimMainViewPresenter>();
-         mainViewPresenter.StartOptions = _startOptions;
-         MainForm = mainViewPresenter.BaseView.DowncastTo<Form>();
+         _mainViewPresenter = _applicationController.Start<IMainViewPresenter>().DowncastTo<IPKSimMainViewPresenter>();
+         Application.AddMessageFilter(this);
+         _mainViewPresenter.StartOptions = _startOptions;
+         MainForm = _mainViewPresenter.BaseView.DowncastTo<Form>();
          _eventPublisher.RemoveListener(_splashPresenter);
          _splashPresenter = null;
       }
@@ -63,6 +66,31 @@ namespace PKSim.UI.BootStrapping
             HideSplashScreen();
             throw;
          }
+      }
+
+      public bool PreFilterMessage(ref Message m)
+      {
+         if (!isWindowsKeyDownMessage(m))
+            return false;
+
+         switch (keyCodeFrom(m))
+         {
+            case (int)(Keys.Control | Keys.W):
+               _mainViewPresenter.ActivePresenter?.Close();
+               return true;
+         }
+
+         return false;
+      }
+
+      private static int keyCodeFrom(Message m)
+      {
+         return (int)m.WParam | (int)Control.ModifierKeys;
+      }
+
+      private static bool isWindowsKeyDownMessage(Message m)
+      {
+         return m.Msg == 256;
       }
    }
 }
