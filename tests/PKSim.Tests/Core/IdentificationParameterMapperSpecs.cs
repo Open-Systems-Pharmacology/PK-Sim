1022a4fc96befd8ff6da6564edd094be0d0f4c70
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-04-29 13:52:45 -0400

    Fixes #3069 Parameters under Applications are not found when importing from snapshot created with ≤ V11 (Simulations and PIs)

--- tests/PKSim.Tests/Core/IdentificationParameterMapperSpecs.cs
+++ tests/PKSim.Tests/Core/IdentificationParameterMapperSpecs.cs
@@ -4,10 +4,13 @@ using FakeItEasy;
 using Microsoft.Extensions.Logging;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core;
 using OSPSuite.Core.Domain;
 using OSPSuite.Core.Domain.ParameterIdentifications;
 using OSPSuite.Core.Domain.Services.ParameterIdentifications;
 using OSPSuite.Core.Services;
+using OSPSuite.Utility.Extensions;
+using PKSim.Assets;
 using PKSim.Core.Model;
 using PKSim.Core.Snapshots.Mappers;
 using Parameter = PKSim.Core.Snapshots.Parameter;
@@ -19,13 +22,16 @@ namespace PKSim.Core
       protected ParameterMapper _parameterMapper;
       protected IdentificationParameter _identificationParameter;
       protected Snapshots.IdentificationParameter _snapshot;
-      private Simulation _simulation;
+      protected Simulation _simulation;
       private IParameter _parameter1;
       private IParameter _parameter2;
+      private IParameter _parameter3;
       private IParameter _startValueParameter;
       protected Parameter _snapshotStartValueParameter;
       protected ParameterSelection _parameterSelection1;
       protected ParameterSelection _parameterSelection2;
+      private ParameterSelection _parameterSelection3;
+      private ParameterSelection _gfrSelection;
       protected IIdentificationParameterFactory _identificationParameterFactory;
       protected IOSPSuiteLogger _logger;
       protected ParameterIdentificationContext _parameterIdentificationContext;
@@ -33,6 +39,9 @@ namespace PKSim.Core
       private PKSimProject _project;
       protected IIdentificationParameterTask _identificationParameterTask;
       private SnapshotContext _snapshotContext;
+      private IParameter _gfrFraction;
+      private IParameter _renalClearances;
+      private ParameterSelection _renalSelection;
 
       protected override Task Context()
       {
@@ -56,24 +65,53 @@ namespace PKSim.Core
          _identificationParameter.Name = "PARAM";
          _parameter1 = DomainHelperForSpecs.ConstantParameterWithValue().WithName("P1");
          _parameter2 = DomainHelperForSpecs.ConstantParameterWithValue().WithName("P2");
+         _parameter3 = DomainHelperForSpecs.ConstantParameterWithValue().WithName("P3");
+         _gfrFraction = DomainHelperForSpecs.ConstantParameterWithValue().WithName("P4");
+         _renalClearances = DomainHelperForSpecs.ConstantParameterWithValue().WithName("P5");
          _simulation = A.Fake<Simulation>().WithName("S");
-         _simulation.Model.Root = new Container {_parameter1, _parameter2};
+         _simulation.Model.Root = new Container {
+            _parameter1, 
+            _parameter2, 
+            new Container{ _parameter3 }.WithName(CoreConstants.ContainerName.Applications),
+            new Container
+            {
+               new Container
+               {
+                  _gfrFraction
+               }.WithName("Glomerular Filtration-GFR"),
+               new Container
+               {
+                  _renalClearances
+               }.WithName("Renal Clearances-test")
+            }.WithName("Alprazolam")
+         };
 
          _identificationParameter.Scaling = Scalings.Linear;
          _parameterSelection1 = new ParameterSelection(_simulation, _parameter1.Name);
          _parameterSelection2 = new ParameterSelection(_simulation, _parameter2.Name);
+         _parameterSelection3 = new ParameterSelection(_simulation, new ObjectPath(CoreConstants.ContainerName.Applications, _parameter3.Name));
+         _gfrSelection = new ParameterSelection(_simulation, new ObjectPath("Alprazolam", "Glomerular Filtration-GFR", _gfrFraction.Name));
+         _renalSelection = new ParameterSelection(_simulation, new ObjectPath("Alprazolam", "Renal Clearances-test", _renalClearances.Name));
          _identificationParameter.AddLinkedParameter(_parameterSelection1);
          _identificationParameter.AddLinkedParameter(_parameterSelection2);
+         _identificationParameter.AddLinkedParameter(_parameterSelection3);
+         _identificationParameter.AddLinkedParameter(_gfrSelection);
+         _identificationParameter.AddLinkedParameter(_renalSelection);
 
          _snapshotStartValueParameter = new Parameter();
          A.CallTo(() => _parameterMapper.MapToSnapshot(_startValueParameter)).Returns(_snapshotStartValueParameter);
 
          _project = new PKSimProject();
          _project.AddBuildingBlock(_simulation);
-         _snapshotContext = new SnapshotContext(_project, ProjectVersions.Current);
+         _snapshotContext = new SnapshotContext(_project, GetSnapshotContextVersion());
          _parameterIdentification = new ParameterIdentification();
          _parameterIdentificationContext = new ParameterIdentificationContext(_parameterIdentification, _snapshotContext);
          return _completed;
+      }
+
+      protected virtual ProjectVersion GetSnapshotContextVersion()
+      {
+         return ProjectVersions.Current;
       }
    }
 
@@ -165,6 +203,72 @@ namespace PKSim.Core
       public void should_log_a_warning()
       {
          A.CallTo(() => _logger.AddToLog(A<string>._, LogLevel.Warning, A<string>._)).MustHaveHappened();
+      }
+   }
+
+   public class When_updating_a_v11_snapshot : concern_for_IdentificationParameterMapper
+   {
+      private IdentificationParameter _newParameterIdentification;
+
+      protected override async Task Context()
+      {
+         await base.Context();
+         _snapshot = await sut.MapToSnapshot(_identificationParameter);
+         A.CallTo(() => _identificationParameterFactory.CreateFor(A<IEnumerable<ParameterSelection>>._, _parameterIdentificationContext.ParameterIdentification)).ReturnsLazily(x => createNewIdentificationParameter(x.Arguments.Get<IEnumerable<ParameterSelection>>(0)));
+         renameApplications(_simulation.Model);
+         renameRenalClearances(_simulation.Model);
+      }
+
+      protected override ProjectVersion GetSnapshotContextVersion()
+      {
+         return ProjectVersions.V11;
+      }
+
+      private IdentificationParameter createNewIdentificationParameter(IEnumerable<ParameterSelection> parameterSelections)
+      {
+         var newIdentificationParameter = new IdentificationParameter();
+
+         parameterSelections.Each(x =>
+         {
+
+            newIdentificationParameter.AddLinkedParameter(x);
+         });
+         return newIdentificationParameter;
+      }
+
+      private void renameRenalClearances(IModel model)
+      {
+         model.Root.GetAllChildren<IContainer>().Each(container =>
+         {
+            if (container.Name.Equals("Glomerular Filtration-GFR"))
+               container.Name = "Glomerular Filtration-GFR-Alprazolam";
+
+            if (container.Name.Equals("Renal Clearances-test"))
+               container.Name = "Renal Clearances-test-Alprazolam";
+         });
+      }
+
+      private static void renameApplications(IModel model)
+      {
+         model.Root.GetAllChildren<IContainer>().Each(container =>
+         {
+            if (container.Name.Equals(CoreConstants.ContainerName.Applications))
+               container.Name = "Events";
+         });
+      }
+
+      protected override async Task Because()
+      {
+         _newParameterIdentification = await sut.MapToModel(_snapshot, _parameterIdentificationContext);
+      }
+
+      [Observation]
+      public void should_change_the_parameter_selection_paths()
+      {
+         _newParameterIdentification.AllLinkedParameters.Count.ShouldBeEqualTo(5);
+         _newParameterIdentification.AllLinkedParameters[2].PathArray.ShouldContain("Events");
+         _newParameterIdentification.AllLinkedParameters[3].PathArray.ShouldContain("Glomerular Filtration-GFR-Alprazolam");
+         _newParameterIdentification.AllLinkedParameters[4].PathArray.ShouldContain("Renal Clearances-test-Alprazolam");
       }
    }
 }
