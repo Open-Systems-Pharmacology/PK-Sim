b85122ef2cb8f585914c6f2ec0ecccbaaccba846
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-05-22 17:53:14 -0400

    Fixes #3130 Transfer rebuilt SimulationTransfer by serialized string (#3131)

--- src/PKSim.UI.Starter/SimulationTransferConstructor.cs
+++ src/PKSim.UI.Starter/SimulationTransferConstructor.cs
@@ -9,11 +9,14 @@ namespace PKSim.UI.Starter
       {
          var container = ApplicationStartup.Initialize();
          var projectSnapshotToSimulationMapper = container.Resolve<IProjectSnapshotToSimulationMapper>();
+         var moBiExportTask = container.Resolve<IMoBiExportTask>();
          var modelCoreSimulation = projectSnapshotToSimulationMapper.MapFrom(projectSnapshot);
-         return new SimulationTransfer
+         var transfer =  new SimulationTransfer
          {
             Simulation = modelCoreSimulation,
          };
+
+         return moBiExportTask.Serialize(transfer);
       }
    }
 }
