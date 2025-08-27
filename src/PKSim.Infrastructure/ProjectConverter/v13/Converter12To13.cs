b75013c26b5126eaf3a2f50b68b57634d0461d09
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-27 14:07:20 -0400

    add empty project conversion

---
+++ src/PKSim.Infrastructure/ProjectConverter/v13/Converter12To13.cs
@@ -0,0 +1,20 @@
+﻿using PKSim.Core;
+using System.Xml.Linq;
+
+namespace PKSim.Infrastructure.ProjectConverter.v13
+{
+   public class Converter11To12 : IObjectConverter
+   {
+      public bool IsSatisfiedBy(int version) => version == ProjectVersions.V12;
+
+      public (int convertedToVersion, bool conversionHappened) Convert(object objectToConvert, int originalVersion)
+      {
+         return (ProjectVersions.V13, false);
+      }
+
+      public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element, int originalVersion)
+      {
+         return (ProjectVersions.V13, false);
+      }
+   }
+}
