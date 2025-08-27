2cfb5088478248fb2da6f7636329b797d4d2cd67
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-27 14:12:08 -0400

    comment

--- src/PKSim.Infrastructure/ProjectConverter/v13/Converter12To13.cs
+++ src/PKSim.Infrastructure/ProjectConverter/v13/Converter12To13.cs
@@ -3,8 +3,10 @@ using System.Xml.Linq;
 
 namespace PKSim.Infrastructure.ProjectConverter.v13
 {
-   public class Converter11To12 : IObjectConverter
+   public class Converter12To13 : IObjectConverter
    {
+      // 13.0 is not compatible with 12.0, but you don't need an explicit conversion to move forward.
+      // To satisfy the next converter, the object must pass through v13.0 conversion
       public bool IsSatisfiedBy(int version) => version == ProjectVersions.V12;
 
       public (int convertedToVersion, bool conversionHappened) Convert(object objectToConvert, int originalVersion)
