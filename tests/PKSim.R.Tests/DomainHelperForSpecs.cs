41c605c67fb58cd478cce8410c983363e7cfb1ff
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-02-10 19:29:07 -0500

    pr feedback

--- tests/PKSim.R.Tests/DomainHelperForSpecs.cs
+++ tests/PKSim.R.Tests/DomainHelperForSpecs.cs
@@ -5,7 +5,6 @@ namespace PKSim.R;
 
 internal static class DomainHelperForSpecs
 {
-   // Borrowing test data from PKSim.Tests
    private static readonly string _pathToData = "..\\..\\..\\Data\\";
 
    public static string DataFilePathFor(string fileNameWithExtension) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _pathToData, fileNameWithExtension);
