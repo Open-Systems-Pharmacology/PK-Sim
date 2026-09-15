f32fa91075f1495bbd5b9b075e398851cd591dbb
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-09-15 13:31:40 -0400

    Fixes #3758 Exchange building blocks created for MoBi as serialized PKML (#3760)

---
+++ src/PKSim.Starter/ExchangeSerializer.cs
@@ -0,0 +1,11 @@
+﻿using OSPSuite.Core.Serialization.Xml;
+using IContainer = OSPSuite.Utility.Container.IContainer;
+
+namespace PKSim.Starter;
+
+// All objects handed to MoBi are exchanged as serialized PKML so that they are recreated in the MoBi container
+// (MoBi dimensions, MoBiSpatialStructures instead of PKSimSpatialStructures, ...)
+internal static class ExchangeSerializer
+{
+   internal static string Serialize<T>(T itemToSerialize, IContainer container) => container.Resolve<IPKMLPersistor>().Serialize(itemToSerialize);
+}
